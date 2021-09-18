97529eda137b90d77528a267fd858cf3d9dd4c3c
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2021-09-18 23:49:26 +0200

    Refactored DupBox

--- DS4Windows/DS4Forms/DupBox.xaml.cs
+++ DS4Windows/DS4Forms/DupBox.xaml.cs
@@ -1,45 +1,36 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using DS4Windows;
 
 namespace DS4WinWPF.DS4Forms
 {
     /// <summary>
-    /// Interaction logic for DupBox.xaml
+    ///     Interaction logic for DupBox.xaml
     /// </summary>
     public partial class DupBox : UserControl
     {
-        private string oldfilename;
-        public string OldFilename { get => oldfilename; set => oldfilename = value; }
-
-        public event EventHandler Cancel;
         public delegate void SaveHandler(DupBox sender, string profilename);
-        public event SaveHandler Save;
 
         public DupBox()
         {
             InitializeComponent();
         }
 
+        public string OldFilename { get; set; }
+
+        public event EventHandler Cancel;
+        public event SaveHandler Save;
+
         private void SaveBtn_Click(object sender, RoutedEventArgs e)
         {
-            string profile = profileTxt.Text;
+            var profile = profileTxt.Text;
             if (!string.IsNullOrWhiteSpace(profile) &&
-                profile.IndexOfAny(System.IO.Path.GetInvalidFileNameChars()) == -1)
+                profile.IndexOfAny(Path.GetInvalidFileNameChars()) == -1)
             {
-                System.IO.File.Copy(DS4Windows.Global.RuntimeAppDataPath + "\\Profiles\\" + oldfilename + ".xml",
-                DS4Windows.Global.RuntimeAppDataPath + "\\Profiles\\" + profile + ".xml", true);
+                File.Copy(Path.Combine(Global.RuntimeAppDataPath, "Profiles", OldFilename, ".xml"),
+                    Path.Combine(Global.RuntimeAppDataPath, "Profiles", profile, ".xml"), true);
                 Save?.Invoke(this, profile);
             }
             else
