c6f29070421fa315e31b04382f2f0253ff156f35
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2021-09-21 14:40:52 +0200

    Refactored DS4OutDevice

--- DS4Windows/DS4Control/DS4OutDevice.cs
+++ DS4Windows/DS4Control/DS4OutDevice.cs
@@ -1,24 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using Nefarius.ViGEm.Client;
 using Nefarius.ViGEm.Client.Targets;
-using Nefarius.ViGEm.Client.Targets.DualShock4;
 
 namespace DS4Windows
 {
-    abstract class DS4OutDevice : OutputDevice
+    internal abstract class DS4OutDevice : OutputDevice
     {
         public const string devtype = "DS4";
 
         public IDualShock4Controller cont;
+
         //public DualShock4FeedbackReceivedEventHandler forceFeedbackCall;
         public Dictionary<int, DualShock4FeedbackReceivedEventHandler> forceFeedbacksDict =
-            new Dictionary<int, DualShock4FeedbackReceivedEventHandler>();
+            new();
 
-        public DS4OutDevice(ViGEmClient client)
+        protected DS4OutDevice(ViGEmClient client)
         {
             cont = client.CreateDualShock4Controller();
             //cont = client.CreateDualShock4Controller(0x054C, 0x09CC);
@@ -30,12 +26,10 @@ namespace DS4Windows
             cont.Connect();
             connected = true;
         }
+
         public override void Disconnect()
         {
-            foreach (KeyValuePair<int, DualShock4FeedbackReceivedEventHandler> pair in forceFeedbacksDict)
-            {
-                cont.FeedbackReceived -= pair.Value;
-            }
+            foreach (var pair in forceFeedbacksDict) cont.FeedbackReceived -= pair.Value;
 
             forceFeedbacksDict.Clear();
 
@@ -44,21 +38,22 @@ namespace DS4Windows
             //cont.Dispose();
             cont = null;
         }
-        public override string GetDeviceType() => devtype;
+
+        public override string GetDeviceType()
+        {
+            return devtype;
+        }
 
         public override void RemoveFeedbacks()
         {
-            foreach (KeyValuePair<int, DualShock4FeedbackReceivedEventHandler> pair in forceFeedbacksDict)
-            {
-                cont.FeedbackReceived -= pair.Value;
-            }
+            foreach (var pair in forceFeedbacksDict) cont.FeedbackReceived -= pair.Value;
 
             forceFeedbacksDict.Clear();
         }
 
         public override void RemoveFeedback(int inIdx)
         {
-            if (forceFeedbacksDict.TryGetValue(inIdx, out DualShock4FeedbackReceivedEventHandler handler))
+            if (forceFeedbacksDict.TryGetValue(inIdx, out var handler))
             {
                 cont.FeedbackReceived -= handler;
                 forceFeedbacksDict.Remove(inIdx);
