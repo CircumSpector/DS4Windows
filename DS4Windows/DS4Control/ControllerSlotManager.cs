c3272bb97cffda57ce29c7377ac811a8de29b3de
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2021-09-21 14:09:01 +0200

    Refactored ControllerSlotManager

--- DS4Windows/DS4Control/ControllerSlotManager.cs
+++ DS4Windows/DS4Control/ControllerSlotManager.cs
@@ -1,56 +1,52 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Threading;
 
 namespace DS4Windows
 {
     public class ControllerSlotManager
     {
-        private ReaderWriterLockSlim collectionLocker = new ReaderWriterLockSlim();
-        public ReaderWriterLockSlim CollectionLocker { get => collectionLocker; }
-
-        private List<DS4Device> controllerColl;
-        public List<DS4Device> ControllerColl { get => controllerColl; set => controllerColl = value; }
-        
-        private Dictionary<int, DS4Device> controllerDict;
-        private Dictionary<DS4Device, int> reverseControllerDict;
-        public Dictionary<int, DS4Device> ControllerDict { get => controllerDict; }
-        public Dictionary<DS4Device, int> ReverseControllerDict { get => reverseControllerDict; }
-
         public ControllerSlotManager()
         {
-            controllerColl = new List<DS4Device>();
-            controllerDict = new Dictionary<int, DS4Device>();
-            reverseControllerDict = new Dictionary<DS4Device, int>();
+            ControllerColl = new List<DS4Device>();
+            ControllerDict = new Dictionary<int, DS4Device>();
+            ReverseControllerDict = new Dictionary<DS4Device, int>();
         }
+
+        public ReaderWriterLockSlim CollectionLocker { get; } = new();
+
+        public List<DS4Device> ControllerColl { get; set; }
+
+        public Dictionary<int, DS4Device> ControllerDict { get; }
+
+        public Dictionary<DS4Device, int> ReverseControllerDict { get; }
 
         public void AddController(DS4Device device, int slotIdx)
         {
-            using (WriteLocker locker = new WriteLocker(collectionLocker))
+            using (var locker = new WriteLocker(CollectionLocker))
             {
-                controllerColl.Add(device);
-                controllerDict.Add(slotIdx, device);
-                reverseControllerDict.Add(device, slotIdx);
+                ControllerColl.Add(device);
+                ControllerDict.Add(slotIdx, device);
+                ReverseControllerDict.Add(device, slotIdx);
             }
         }
 
         public void RemoveController(DS4Device device, int slotIdx)
         {
-            using (WriteLocker locker = new WriteLocker(collectionLocker))
+            using (var locker = new WriteLocker(CollectionLocker))
             {
-                controllerColl.Remove(device);
-                controllerDict.Remove(slotIdx);
-                reverseControllerDict.Remove(device);
+                ControllerColl.Remove(device);
+                ControllerDict.Remove(slotIdx);
+                ReverseControllerDict.Remove(device);
             }
         }
 
         public void ClearControllerList()
         {
-            using (WriteLocker locker = new WriteLocker(collectionLocker))
+            using (var locker = new WriteLocker(CollectionLocker))
             {
-                controllerColl.Clear();
-                controllerDict.Clear();
-                reverseControllerDict.Clear();
+                ControllerColl.Clear();
+                ControllerDict.Clear();
+                ReverseControllerDict.Clear();
             }
         }
     }
