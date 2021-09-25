2f96a80e0091d77b36a1179a3cf98b574eb7c8ce
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2021-09-25 21:10:46 +0200

    Added missing default values

--- DS4Windows/DS4Control/Profiles/Legacy/ApplicationSettingsSchema.ChildTypes.cs
+++ DS4Windows/DS4Control/Profiles/Legacy/ApplicationSettingsSchema.ChildTypes.cs
@@ -6,59 +6,54 @@ namespace DS4WinWPF.DS4Control.Profiles.Legacy
     [XmlRoot(ElementName = "UDPServerSmoothingOptions")]
     public class UDPServerSmoothingOptions
     {
-
         [XmlElement(ElementName = "UseSmoothing")]
-        public bool UseSmoothing { get; set; }
+        public bool UseSmoothing { get; set; } = false;
 
         [XmlElement(ElementName = "UdpSmoothMinCutoff")]
-        public double UdpSmoothMinCutoff { get; set; }
+        public double UdpSmoothMinCutoff { get; set; } = 0.4f;
 
         [XmlElement(ElementName = "UdpSmoothBeta")]
-        public double UdpSmoothBeta { get; set; }
+        public double UdpSmoothBeta { get; set; } = 0.2f;
     }
 
     [XmlRoot(ElementName = "DS4SupportSettings")]
     public class DS4SupportSettings
     {
-
-        [XmlElement(ElementName = "Enabled")]
-        public bool Enabled { get; set; }
+        [XmlElement(ElementName = "Enabled")] 
+        public bool Enabled { get; set; } = true;
     }
 
     [XmlRoot(ElementName = "DualSenseSupportSettings")]
     public class DualSenseSupportSettings
     {
-
-        [XmlElement(ElementName = "Enabled")]
-        public bool Enabled { get; set; }
+        [XmlElement(ElementName = "Enabled")] 
+        public bool Enabled { get; set; } = true;
     }
 
     [XmlRoot(ElementName = "SwitchProSupportSettings")]
     public class SwitchProSupportSettings
     {
-
-        [XmlElement(ElementName = "Enabled")]
-        public bool Enabled { get; set; }
+        [XmlElement(ElementName = "Enabled")] 
+        public bool Enabled { get; set; } = true;
     }
 
     [XmlRoot(ElementName = "JoyConSupportSettings")]
     public class JoyConSupportSettings
     {
-
-        [XmlElement(ElementName = "Enabled")]
-        public bool Enabled { get; set; }
+        [XmlElement(ElementName = "Enabled")] 
+        public bool Enabled { get; set; } = true;
 
         [XmlElement(ElementName = "LinkMode")]
-        public JoyConDeviceOptions.LinkMode LinkMode { get; set; }
+        public JoyConDeviceOptions.LinkMode LinkMode { get; set; } = JoyConDeviceOptions.LinkMode.Joined;
 
         [XmlElement(ElementName = "JoinedGyroProvider")]
-        public JoyConDeviceOptions.JoinedGyroProvider JoinedGyroProvider { get; set; }
+        public JoyConDeviceOptions.JoinedGyroProvider JoinedGyroProvider { get; set; } =
+            JoyConDeviceOptions.JoinedGyroProvider.JoyConR;
     }
 
     [XmlRoot(ElementName = "DeviceOptions")]
     public class DeviceOptions
     {
-
         [XmlElement(ElementName = "DS4SupportSettings")]
         public DS4SupportSettings DS4SupportSettings { get; set; } = new();
 
