3f5c20e969e0eee7013bcf97efd0e960dad9f267
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2021-09-30 09:52:43 +0200

    Refactored AutoProfileHolder

--- DS4Windows/AutoProfileHolder.cs
+++ DS4Windows/AutoProfileHolder.cs
@@ -1,81 +1,74 @@
-﻿using DS4Windows;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.Collections.ObjectModel;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Data;
 using System.Xml;
+using DS4Windows;
 
 namespace DS4WinWPF
 {
     public class AutoProfileHolder
     {
-        private object _colLockobj = new object();
-        private ObservableCollection<AutoProfileEntity> autoProfileColl;
-        public ObservableCollection<AutoProfileEntity> AutoProfileColl { get => autoProfileColl; }
+        private readonly object _colLockobj = new();
+
         //public Dictionary<string, AutoProfileEntity> AutoProfileDict { get => autoProfileDict; }
 
         //private Dictionary<string, AutoProfileEntity> autoProfileDict;
 
         public AutoProfileHolder()
         {
-            autoProfileColl = new ObservableCollection<AutoProfileEntity>();
+            AutoProfileColl = new ObservableCollection<AutoProfileEntity>();
             //autoProfileDict = new Dictionary<string, AutoProfileEntity>();
             Load();
 
-            BindingOperations.EnableCollectionSynchronization(autoProfileColl, _colLockobj);
+            BindingOperations.EnableCollectionSynchronization(AutoProfileColl, _colLockobj);
         }
+
+        public ObservableCollection<AutoProfileEntity> AutoProfileColl { get; }
 
         [ConfigurationSystemComponent]
         private void Load()
         {
             try
             {
-                XmlDocument doc = new XmlDocument();
+                var doc = new XmlDocument();
 
                 if (!File.Exists(Path.Combine(Global.RuntimeAppDataPath, Constants.AutoProfilesFileName)))
                     return;
 
-                doc.Load(Path.Combine( Global.RuntimeAppDataPath, Constants.AutoProfilesFileName));
-                XmlNodeList programslist = doc.SelectNodes("Programs/Program");
+                doc.Load(Path.Combine(Global.RuntimeAppDataPath, Constants.AutoProfilesFileName));
+                var programslist = doc.SelectNodes("Programs/Program");
                 foreach (XmlNode x in programslist)
                 {
-                    string path = x.Attributes["path"]?.Value ?? string.Empty;
-                    string title = x.Attributes["title"]?.Value ?? string.Empty;
-                    AutoProfileEntity autoprof = new AutoProfileEntity(path, title);
+                    var path = x.Attributes["path"]?.Value ?? string.Empty;
+                    var title = x.Attributes["title"]?.Value ?? string.Empty;
+                    var autoprof = new AutoProfileEntity(path, title);
 
                     XmlNode item;
-                    for (int i = 0; i < ControlService.CURRENT_DS4_CONTROLLER_LIMIT; i++)
+                    for (var i = 0; i < ControlService.CURRENT_DS4_CONTROLLER_LIMIT; i++)
                     {
-                        item = x.SelectSingleNode($"Controller{i+1}");
-                        if (item != null)
-                        {
-                            autoprof.ProfileNames[i] = item.InnerText;
-                        }
+                        item = x.SelectSingleNode($"Controller{i + 1}");
+                        if (item != null) autoprof.ProfileNames[i] = item.InnerText;
                     }
 
-                    item = x.SelectSingleNode($"TurnOff");
-                    if (item != null && bool.TryParse(item.InnerText, out bool turnoff))
-                    {
-                        autoprof.Turnoff = turnoff;
-                    }
+                    item = x.SelectSingleNode("TurnOff");
+                    if (item != null && bool.TryParse(item.InnerText, out var turnoff)) autoprof.Turnoff = turnoff;
 
-                    autoProfileColl.Add(autoprof);
+                    AutoProfileColl.Add(autoprof);
                     //autoProfileDict.Add(path, autoprof);
                 }
             }
-            catch (Exception) { }
+            catch (Exception)
+            {
+            }
         }
 
         [ConfigurationSystemComponent]
         public bool Save(string m_Profile)
         {
-            XmlDocument doc = new XmlDocument();
+            var doc = new XmlDocument();
             XmlNode Node;
-            bool saved = true;
+            var saved = true;
             try
             {
                 Node = doc.CreateXmlDeclaration("1.0", "utf-8", string.Empty);
@@ -89,14 +82,11 @@ namespace DS4WinWPF
 
                 Node = doc.CreateNode(XmlNodeType.Element, "Programs", "");
                 doc.AppendChild(Node);
-                foreach (AutoProfileEntity entity in autoProfileColl)
+                foreach (var entity in AutoProfileColl)
                 {
-                    XmlElement el = doc.CreateElement("Program");
+                    var el = doc.CreateElement("Program");
                     el.SetAttribute("path", entity.Path);
-                    if (!string.IsNullOrEmpty(entity.Title))
-                    {
-                        el.SetAttribute("title", entity.Title);
-                    }
+                    if (!string.IsNullOrEmpty(entity.Title)) el.SetAttribute("title", entity.Title);
 
                     el.AppendChild(doc.CreateElement("Controller1")).InnerText = entity.ProfileNames[0];
                     el.AppendChild(doc.CreateElement("Controller2")).InnerText = entity.ProfileNames[1];
@@ -109,6 +99,7 @@ namespace DS4WinWPF
                         el.AppendChild(doc.CreateElement("Controller7")).InnerText = entity.ProfileNames[6];
                         el.AppendChild(doc.CreateElement("Controller8")).InnerText = entity.ProfileNames[7];
                     }
+
                     el.AppendChild(doc.CreateElement("TurnOff")).InnerText = entity.Turnoff.ToString();
 
                     Node.AppendChild(el);
@@ -116,32 +107,29 @@ namespace DS4WinWPF
 
                 doc.Save(m_Profile);
             }
-            catch (Exception) { saved = false; }
+            catch (Exception)
+            {
+                saved = false;
+            }
+
             return saved;
         }
 
         public void Remove(AutoProfileEntity item)
         {
             //autoProfileDict.Remove(item.Path);
-            autoProfileColl.Remove(item);
+            AutoProfileColl.Remove(item);
         }
     }
 
     public class AutoProfileEntity
     {
-        public string path = string.Empty;
-        public string title = string.Empty;
-        private string path_lowercase;
-        private string title_lowercase;
-        private bool turnoff;
-        private string[] profileNames = new string[DS4Windows.Global.MAX_DS4_CONTROLLER_COUNT] { string.Empty, string.Empty,
-            string.Empty, string.Empty, string.Empty, string.Empty, string.Empty, string.Empty };
         public const string NONE_STRING = "(none)";
+        public string path = string.Empty;
+        private string path_lowercase;
 
-        public string Path { get => path; set => SetSearchPath(value); }
-        public string Title { get => title; set => SetSearchTitle(value); }
-        public bool Turnoff { get => turnoff; set => turnoff = value; }
-        public string[] ProfileNames { get => profileNames; set => profileNames = value; }
+        public string title = string.Empty;
+        private string title_lowercase;
 
         public AutoProfileEntity(string pathStr, string titleStr)
         {
@@ -151,28 +139,42 @@ namespace DS4WinWPF
             SetSearchTitle(titleStr);
         }
 
+        public string Path
+        {
+            get => path;
+            set => SetSearchPath(value);
+        }
+
+        public string Title
+        {
+            get => title;
+            set => SetSearchTitle(value);
+        }
+
+        public bool Turnoff { get; set; }
+
+        public string[] ProfileNames { get; set; } = new string[Global.MAX_DS4_CONTROLLER_COUNT]
+        {
+            string.Empty, string.Empty,
+            string.Empty, string.Empty, string.Empty, string.Empty, string.Empty, string.Empty
+        };
+
         public bool IsMatch(string searchPath, string searchTitle)
         {
-            bool bPathMatched = true;
-            bool bTitleMwatched = true;
+            var bPathMatched = true;
+            var bTitleMwatched = true;
 
             if (!string.IsNullOrEmpty(path_lowercase))
-            {
-                bPathMatched = (path_lowercase == searchPath
-                    || (path[0] == '^' && searchPath.StartsWith(path_lowercase))
-                    || (path[path.Length - 1] == '$' && searchPath.EndsWith(path_lowercase))
-                    || (path[0] == '*' && searchPath.Contains(path_lowercase))
-                   );
-            }
+                bPathMatched = path_lowercase == searchPath
+                               || path[0] == '^' && searchPath.StartsWith(path_lowercase)
+                               || path[path.Length - 1] == '$' && searchPath.EndsWith(path_lowercase)
+                               || path[0] == '*' && searchPath.Contains(path_lowercase);
 
             if (bPathMatched && !string.IsNullOrEmpty(title_lowercase))
-            {
-                bTitleMwatched = (title_lowercase == searchTitle
-                    || (title[0] == '^' && searchTitle.StartsWith(title_lowercase))
-                    || (title[title.Length - 1] == '$' && searchTitle.EndsWith(title_lowercase))
-                    || (title[0] == '*' && searchTitle.Contains(title_lowercase))
-                   );
-            }
+                bTitleMwatched = title_lowercase == searchTitle
+                                 || title[0] == '^' && searchTitle.StartsWith(title_lowercase)
+                                 || title[title.Length - 1] == '$' && searchTitle.EndsWith(title_lowercase)
+                                 || title[0] == '*' && searchTitle.Contains(title_lowercase);
 
             // If both path and title defined in autoprofile entry then do AND condition (ie. both path and title should match)
             return bPathMatched && bTitleMwatched;
@@ -188,11 +190,15 @@ namespace DS4WinWPF
                 if (path.Length >= 2)
                 {
                     if (path[0] == '^') path_lowercase = path_lowercase.Substring(1);
-                    else if (path[path.Length - 1] == '$') path_lowercase = path_lowercase.Substring(0, path_lowercase.Length - 1);
+                    else if (path[path.Length - 1] == '$')
+                        path_lowercase = path_lowercase.Substring(0, path_lowercase.Length - 1);
                     else if (path[0] == '*') path_lowercase = path_lowercase.Substring(1);
                 }
             }
-            else path = path_lowercase = string.Empty;
+            else
+            {
+                path = path_lowercase = string.Empty;
+            }
         }
 
         private void SetSearchTitle(string titleStr)
@@ -205,11 +211,15 @@ namespace DS4WinWPF
                 if (title.Length >= 2)
                 {
                     if (title[0] == '^') title_lowercase = title_lowercase.Substring(1);
-                    else if (title[title.Length - 1] == '$') title_lowercase = title_lowercase.Substring(0, title_lowercase.Length - 1);
+                    else if (title[title.Length - 1] == '$')
+                        title_lowercase = title_lowercase.Substring(0, title_lowercase.Length - 1);
                     else if (title[0] == '*') title_lowercase = title_lowercase.Substring(1);
                 }
             }
-            else title = title_lowercase = string.Empty;
+            else
+            {
+                title = title_lowercase = string.Empty;
+            }
         }
     }
 }
