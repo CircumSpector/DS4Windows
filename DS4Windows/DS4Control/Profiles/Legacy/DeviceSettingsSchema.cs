43d1a90caa382537dc7949a1e71b18ceb0c512df
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2021-09-30 14:58:15 +0200

    Added schema for DS4 and DS5 device options

---
+++ DS4Windows/DS4Control/Profiles/Legacy/DeviceSettingsSchema.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace DS4WinWPF.DS4Control.Profiles.Legacy
+{
+    [XmlRoot(ElementName = "DualSenseSupportSettings")]
+    public class DeviceDualSenseSupportSettings
+    {
+        [XmlElement(ElementName = "EnableRumble")]
+        public bool EnableRumble { get; set; }
+
+        [XmlElement(ElementName = "RumbleStrength")]
+        public string RumbleStrength { get; set; }
+
+        [XmlElement(ElementName = "LEDBarMode")]
+        public string LEDBarMode { get; set; }
+
+        [XmlElement(ElementName = "MuteLEDMode")]
+        public string MuteLEDMode { get; set; }
+    }
+
+    [XmlRoot(ElementName = "Controller")]
+    public class DeviceController
+    {
+        [XmlElement(ElementName = "DualSenseSupportSettings")]
+        public DeviceDualSenseSupportSettings DualSenseSupportSettings { get; set; }
+
+        [XmlAttribute(AttributeName = "Mac")]
+        public string Mac { get; set; }
+
+        [XmlAttribute(AttributeName = "ControllerType")]
+        public string ControllerType { get; set; }
+
+        [XmlText] public string Text { get; set; }
+
+        [XmlElement(ElementName = "DS4SupportSettings")]
+        public DeviceDS4SupportSettings DS4SupportSettings { get; set; }
+    }
+
+    [XmlRoot(ElementName = "DS4SupportSettings")]
+    public class DeviceDS4SupportSettings
+    {
+        [XmlElement(ElementName = "Copycat")] 
+        public bool Copycat { get; set; }
+    }
+
+    [XmlRoot(ElementName = "Controllers")]
+    public class Controllers
+    {
+        [XmlElement(ElementName = "Controller")]
+        public List<DeviceController> Controller { get; set; }
+    }
+}
