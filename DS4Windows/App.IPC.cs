8de8da6633222aa886c0febc0a362edaabebfd42
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2021-10-01 13:37:25 +0200

    Moved IPC stuff to own file

---
+++ DS4Windows/App.IPC.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.IO.MemoryMappedFiles;
+using System.Text;
+using System.Threading;
+
+namespace DS4WinWPF
+{
+    public partial class App
+    {
+        private MemoryMappedViewAccessor ipcClassNameMMA;
+
+        /// <summary>
+        ///     MemoryMappedFile for inter-process communication used to hold className of DS4Form window.
+        /// </summary>
+        private MemoryMappedFile
+            ipcClassNameMMF;
+
+        private MemoryMappedViewAccessor ipcResultDataMMA;
+
+        /// <summary>
+        ///     MemoryMappedFile for inter-process communication used to exchange string result data between cmdline client process
+        ///     and the background running DS4Windows app.
+        /// </summary>
+        private MemoryMappedFile
+            ipcResultDataMMF;
+
+        public void CreateIPCClassNameMMF(IntPtr hWnd)
+        {
+            if (ipcClassNameMMA != null) return; // Already holding a handle to MMF file. No need to re-write the data
+
+            try
+            {
+                var wndClassNameStr = new StringBuilder(128);
+                if (GetClassName(hWnd, wndClassNameStr, wndClassNameStr.Capacity) != 0 && wndClassNameStr.Length > 0)
+                {
+                    var buffer = Encoding.ASCII.GetBytes(wndClassNameStr.ToString());
+
+                    ipcClassNameMMF = MemoryMappedFile.CreateNew("DS4Windows_IPCClassName.dat", 128);
+                    ipcClassNameMMA = ipcClassNameMMF.CreateViewAccessor(0, buffer.Length);
+                    ipcClassNameMMA.WriteArray(0, buffer, 0, buffer.Length);
+                    // The MMF file is alive as long this process holds the file handle open
+                }
+            }
+            catch (Exception)
+            {
+                /* Eat all exceptions because errors here are not fatal for DS4Win */
+            }
+        }
+
+        private string ReadIPCClassNameMMF()
+        {
+            MemoryMappedFile mmf = null;
+            MemoryMappedViewAccessor mma = null;
+
+            try
+            {
+                var buffer = new byte[128];
+                mmf = MemoryMappedFile.OpenExisting("DS4Windows_IPCClassName.dat");
+                mma = mmf.CreateViewAccessor(0, 128);
+                mma.ReadArray(0, buffer, 0, buffer.Length);
+                return Encoding.ASCII.GetString(buffer);
+            }
+            catch (Exception)
+            {
+                // Eat all exceptions
+            }
+            finally
+            {
+                if (mma != null) mma.Dispose();
+                if (mmf != null) mmf.Dispose();
+            }
+
+            return null;
+        }
+
+        private void CreateIPCResultDataMMF()
+        {
+            // Cmdline client process calls this to create the MMF file used in inter-process-communications. The background DS4Windows process 
+            // uses WriteIPCResultDataMMF method to write a command result and the client process reads the result from the same MMF file.
+            if (ipcResultDataMMA != null) return; // Already holding a handle to MMF file. No need to re-write the data
+
+            try
+            {
+                ipcResultDataMMF = MemoryMappedFile.CreateNew("DS4Windows_IPCResultData.dat", 256);
+                ipcResultDataMMA = ipcResultDataMMF.CreateViewAccessor(0, 256);
+                // The MMF file is alive as long this process holds the file handle open
+            }
+            catch (Exception)
+            {
+                /* Eat all exceptions because errors here are not fatal for DS4Win */
+            }
+        }
+
+        private string WaitAndReadIPCResultDataMMF(EventWaitHandle ipcNotifyEvent)
+        {
+            if (ipcResultDataMMA != null)
+                // Wait until the inter-process-communication (IPC) result data is available and read the result
+                try
+                {
+                    // Wait max 10 secs and if the result is still not available then timeout and return "empty" result
+                    if (ipcNotifyEvent == null || ipcNotifyEvent.WaitOne(10000))
+                    {
+                        int strNullCharIdx;
+                        var buffer = new byte[256];
+                        ipcResultDataMMA.ReadArray(0, buffer, 0, buffer.Length);
+                        strNullCharIdx = Array.FindIndex(buffer, byteVal => byteVal == 0);
+                        return Encoding.ASCII.GetString(buffer, 0, strNullCharIdx <= 1 ? 1 : strNullCharIdx);
+                    }
+                }
+                catch (Exception)
+                {
+                    /* Eat all exceptions because errors here are not fatal for DS4Win */
+                }
+
+            return string.Empty;
+        }
+
+        public void WriteIPCResultDataMMF(string dataStr)
+        {
+            // The background DS4Windows process calls this method to write out the result of "-command QueryProfile.device#" command.
+            // The cmdline client process reads the result from the DS4Windows_IPCResultData.dat MMF file and sends the result to console output pipe.
+            MemoryMappedFile mmf = null;
+            MemoryMappedViewAccessor mma = null;
+            EventWaitHandle ipcNotifyEvent = null;
+
+            try
+            {
+                ipcNotifyEvent = EventWaitHandle.OpenExisting("DS4Windows_IPCResultData_ReadyEvent");
+
+                var buffer = Encoding.ASCII.GetBytes(dataStr);
+                mmf = MemoryMappedFile.OpenExisting("DS4Windows_IPCResultData.dat");
+                mma = mmf.CreateViewAccessor(0, 256);
+                mma.WriteArray(0, buffer, 0, buffer.Length >= 256 ? 256 : buffer.Length);
+            }
+            catch (Exception)
+            {
+                // Eat all exceptions
+            }
+            finally
+            {
+                if (mma != null) mma.Dispose();
+                if (mmf != null) mmf.Dispose();
+
+                if (ipcNotifyEvent != null) ipcNotifyEvent.Set();
+            }
+        }
+    }
+}
