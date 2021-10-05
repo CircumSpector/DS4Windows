80dfdd0942cc71e0d755a21f7f455808791e431e
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2021-10-05 18:22:04 +0200

    Styled ControllerRegisterOptionsWindow.xaml

--- DS4Windows/DS4Forms/ControllerRegisterOptionsWindow.xaml.cs
+++ DS4Windows/DS4Forms/ControllerRegisterOptionsWindow.xaml.cs
@@ -1,16 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using AdonisUI.Controls;
 using DS4WinWPF.DS4Forms.ViewModels;
 using DS4Windows;
 
@@ -19,7 +9,7 @@ namespace DS4WinWPF.DS4Forms
     /// <summary>
     /// Interaction logic for ControllerRegisterOptions.xaml
     /// </summary>
-    public partial class ControllerRegisterOptionsWindow : Window
+    public partial class ControllerRegisterOptionsWindow : AdonisWindow
     {
         private ControllerRegDeviceOptsViewModel deviceOptsVM;
 
