aa360be1ca7ad3762a1846fb3307feb92fc2b6d5
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2021-10-06 16:31:42 +0200

    Refurbished PluginOutDevWindow

--- DS4Windows/DS4Forms/PluginOutDevWindow.xaml.cs
+++ DS4Windows/DS4Forms/PluginOutDevWindow.xaml.cs
@@ -1,25 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using AdonisUI.Controls;
 using DS4Windows;
 using DS4WinWPF.DS4Control;
+using MessageBoxResult = System.Windows.MessageBoxResult;
 
 namespace DS4WinWPF.DS4Forms
 {
     /// <summary>
     /// Interaction logic for PluginOutDevWindow.xaml
     /// </summary>
-    public partial class PluginOutDevWindow : Window
+    public partial class PluginOutDevWindow : AdonisWindow
     {
         private MessageBoxResult result = MessageBoxResult.Cancel;
         public MessageBoxResult Result { get => result; }
