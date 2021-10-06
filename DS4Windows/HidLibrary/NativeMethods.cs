8c3b2fa8f5f546b71cf67244b457470ebd5ad820
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2021-10-06 12:06:24 +0200

    Sorted modifiers

--- DS4Windows/HidLibrary/NativeMethods.cs
+++ DS4Windows/HidLibrary/NativeMethods.cs
@@ -17,13 +17,13 @@ namespace DS4Windows
         }
 
         [DllImport("bthprops.cpl", CharSet = CharSet.Auto)]
-        internal extern static IntPtr BluetoothFindFirstRadio(ref BLUETOOTH_FIND_RADIO_PARAMS pbtfrp, ref IntPtr phRadio);
+        internal static extern IntPtr BluetoothFindFirstRadio(ref BLUETOOTH_FIND_RADIO_PARAMS pbtfrp, ref IntPtr phRadio);
 
         [DllImport("bthprops.cpl", CharSet = CharSet.Auto)]
-        internal extern static bool BluetoothFindNextRadio(IntPtr hFind, ref IntPtr phRadio);
+        internal static extern bool BluetoothFindNextRadio(IntPtr hFind, ref IntPtr phRadio);
 
         [DllImport("bthprops.cpl", CharSet = CharSet.Auto)]
-        internal extern static bool BluetoothFindRadioClose(IntPtr hFind);
+        internal static extern bool BluetoothFindRadioClose(IntPtr hFind);
 
         [DllImport("kernel32.dll", SetLastError = true)]
         internal static extern Boolean DeviceIoControl(IntPtr DeviceHandle, UInt32 IoControlCode, ref long InBuffer, Int32 InBufferSize, IntPtr OutBuffer, Int32 OutBufferSize, ref Int32 BytesReturned, IntPtr Overlapped);
@@ -77,30 +77,32 @@ namespace DS4Windows
         }
 
         [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true, CharSet = CharSet.Auto)]
-        static internal extern bool CancelIo(IntPtr hFile);
+        internal static extern bool CancelIo(IntPtr hFile);
 
         [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true, CharSet = CharSet.Auto)]
-        static internal extern bool CancelIoEx(IntPtr hFile, IntPtr lpOverlapped);
+        internal static extern bool CancelIoEx(IntPtr hFile, IntPtr lpOverlapped);
 
         [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true, CharSet = CharSet.Auto)]
-        static internal extern bool CancelSynchronousIo(IntPtr hObject);
+        internal static extern bool CancelSynchronousIo(IntPtr hObject);
 
         [DllImport("kernel32.dll", CharSet = CharSet.Auto)]
-        static internal extern IntPtr CreateEvent(ref SECURITY_ATTRIBUTES securityAttributes, int bManualReset, int bInitialState, string lpName);
+        internal static extern IntPtr CreateEvent(ref SECURITY_ATTRIBUTES securityAttributes, int bManualReset, int bInitialState, string lpName);
 
         [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-        static internal extern IntPtr CreateFile(string lpFileName, uint dwDesiredAccess, int dwShareMode, ref SECURITY_ATTRIBUTES lpSecurityAttributes, int dwCreationDisposition, uint dwFlagsAndAttributes, int hTemplateFile);
+        internal static extern IntPtr CreateFile(string lpFileName, uint dwDesiredAccess, int dwShareMode, ref SECURITY_ATTRIBUTES lpSecurityAttributes, int dwCreationDisposition, uint dwFlagsAndAttributes, int hTemplateFile);
 
         [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
         internal static extern SafeFileHandle CreateFile(String lpFileName, UInt32 dwDesiredAccess, Int32 dwShareMode, IntPtr lpSecurityAttributes, Int32 dwCreationDisposition, UInt32 dwFlagsAndAttributes, Int32 hTemplateFile);
         [DllImport("kernel32.dll", SetLastError = true)]
-        static internal extern bool ReadFile(IntPtr hFile, [Out] byte[] lpBuffer, uint nNumberOfBytesToRead, out uint lpNumberOfBytesRead, IntPtr lpOverlapped);
+        internal static extern bool ReadFile(IntPtr hFile, [Out] byte[] lpBuffer, uint nNumberOfBytesToRead, out uint lpNumberOfBytesRead, IntPtr lpOverlapped);
 
         [DllImport("kernel32.dll")]
-        static internal extern uint WaitForSingleObject(IntPtr hHandle, int dwMilliseconds);
+        internal static extern uint WaitForSingleObject(IntPtr hHandle, int dwMilliseconds);
 
         [DllImport("kernel32.dll")]
-        static internal extern bool WriteFile(IntPtr hFile, byte[] lpBuffer, uint nNumberOfBytesToWrite, out uint lpNumberOfBytesWritten, [In] ref System.Threading.NativeOverlapped lpOverlapped);
+        internal static extern bool WriteFile(IntPtr hFile, byte[] lpBuffer, uint nNumberOfBytesToWrite, out uint lpNumberOfBytesWritten, [In] ref System.Threading.NativeOverlapped lpOverlapped);
+
+
 
         internal const int DBT_DEVICEARRIVAL = 0x8000;
         internal const int DBT_DEVICEREMOVECOMPLETE = 0x8004;
