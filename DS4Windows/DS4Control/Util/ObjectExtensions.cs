a9934593b6d88ff9523d2e04f6f3afe303406c0c
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2021-10-08 00:08:33 +0200

    Added docs

--- DS4Windows/DS4Control/Util/ObjectExtensions.cs
+++ DS4Windows/DS4Control/Util/ObjectExtensions.cs
@@ -1,8 +1,18 @@
 ﻿namespace DS4WinWPF.DS4Control.Util
 {
+    /// <summary>
+    ///     Utility class to copy (not clone) the properties of one type to another.
+    /// </summary>
+    /// <typeparam name="TParent">The source type.</typeparam>
+    /// <typeparam name="TChild">The target type.</typeparam>
     internal class PropertyCopier<TParent, TChild> where TParent : class
         where TChild : class
     {
+        /// <summary>
+        ///     Copies all properties from source to target.
+        /// </summary>
+        /// <param name="parent">The source object.</param>
+        /// <param name="child">The target object.</param>
         public static void Copy(TParent parent, TChild child)
         {
             var parentProperties = parent.GetType().GetProperties();
