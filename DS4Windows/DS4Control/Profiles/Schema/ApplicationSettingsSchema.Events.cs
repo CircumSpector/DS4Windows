8ced840c8fef1ce0cb4cee57f33571a3bca7d204
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2021-10-08 09:25:28 +0200

    Added logic to enable tracing by config switch

--- DS4Windows/DS4Control/Profiles/Schema/ApplicationSettingsSchema.Events.cs
+++ DS4Windows/DS4Control/Profiles/Schema/ApplicationSettingsSchema.Events.cs
@@ -1,14 +1,41 @@
 ﻿using System;
+using Jaeger;
+using Jaeger.Samplers;
+using Jaeger.Senders;
+using Jaeger.Senders.Thrift;
+using JetBrains.Annotations;
+using Microsoft.Extensions.Logging.Abstractions;
+using OpenTracing.Util;
 
 namespace DS4WinWPF.DS4Control.Profiles.Schema
 {
     public partial class DS4WindowsAppSettings
     {
-        public event Action IsTracingEnabledChanged;
+        public event Action<bool> IsTracingEnabledChanged;
 
-        private void OnIsTracingEnabledChanged()
+        [UsedImplicitly]
+        private void OnIsTracingEnabledChanged(object oldValue, object newValue)
         {
-            IsTracingEnabledChanged?.Invoke();
+            //
+            // Automatically register tracer, if configuration value instructs to
+            // 
+            if ((bool)newValue && !GlobalTracer.IsRegistered())
+            {
+                // This is necessary to pick the correct sender, otherwise a NoopSender is used!
+                Configuration.SenderConfiguration.DefaultSenderResolver = new SenderResolver(new NullLoggerFactory())
+                    .RegisterSenderFactory<ThriftSenderFactory>();
+
+                // This will log to a default localhost installation of Jaeger.
+                var tracer = new Tracer.Builder(DS4Windows.Constants.ApplicationName)
+                    .WithLoggerFactory(new NullLoggerFactory())
+                    .WithSampler(new ConstSampler(true))
+                    .Build();
+
+                // Allows code that can't use DI to also access the tracer.
+                GlobalTracer.Register(tracer);
+            }
+
+            IsTracingEnabledChanged?.Invoke((bool)newValue);
         }
     }
 }
