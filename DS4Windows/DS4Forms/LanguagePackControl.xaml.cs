09199196b0a151ad10e549075c5a01e798276d01
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2021-10-10 01:22:55 +0200

    Changed to styled message box

--- DS4Windows/DS4Forms/LanguagePackControl.xaml.cs
+++ DS4Windows/DS4Forms/LanguagePackControl.xaml.cs
@@ -1,23 +1,24 @@
 ﻿using System;
-using System.Windows;
 using System.Windows.Controls;
+using AdonisUI.Controls;
+using DS4Windows;
 using DS4WinWPF.DS4Forms.ViewModels;
 
 namespace DS4WinWPF.DS4Forms
 {
     /// <summary>
-    /// Interaction logic for LanguagePackControl.xaml
+    ///     Interaction logic for LanguagePackControl.xaml
     /// </summary>
     public partial class LanguagePackControl : UserControl
     {
-        private LanguagePackViewModel langPackVM;
+        private readonly LanguagePackViewModel langPackVM;
 
         public LanguagePackControl()
         {
             InitializeComponent();
 
             langPackVM = new LanguagePackViewModel();
-            this.DataContext = null;
+            DataContext = null;
             langPackVM.ScanFinished += LangPackVM_ScanFinished;
             langPackVM.SelectedIndexChanged += CheckForCultureChange;
 
@@ -27,10 +28,8 @@ namespace DS4WinWPF.DS4Forms
         private void CheckForCultureChange(object sender, EventArgs e)
         {
             if (langPackVM.ChangeLanguagePack())
-            {
                 MessageBox.Show(Properties.Resources.LanguagePackApplyRestartRequired,
-                    "DS4Windows", MessageBoxButton.OK, MessageBoxImage.Information);
-            }
+                    Constants.ApplicationName, MessageBoxButton.OK, MessageBoxImage.Information);
         }
 
         private void LangPackVM_ScanFinished(object sender, EventArgs e)
@@ -38,7 +37,7 @@ namespace DS4WinWPF.DS4Forms
             Dispatcher.BeginInvoke((Action)(() =>
             {
                 cbCulture.IsEnabled = true;
-                this.DataContext = langPackVM;
+                DataContext = langPackVM;
             }));
         }
     }
