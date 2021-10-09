5a60799b03a6e35a582e3044c255eafb9b0bfde0
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2021-10-09 15:41:19 +0200

    Minor refactoring

--- DS4Windows/DS4Forms/ViewModels/LanguagePackViewModel.cs
+++ DS4Windows/DS4Forms/ViewModels/LanguagePackViewModel.cs
@@ -4,7 +4,6 @@ using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Reflection;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using DS4Windows;
