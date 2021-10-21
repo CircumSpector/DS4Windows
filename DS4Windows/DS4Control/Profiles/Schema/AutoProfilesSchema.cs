78558c3b557cf1b219474ae71572a69fab034824
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2021-10-21 13:29:57 +0200

    Designed new AutoSwitchingProfile

--- DS4Windows/DS4Control/Profiles/Schema/AutoProfilesSchema.cs
+++ DS4Windows/DS4Control/Profiles/Schema/AutoProfilesSchema.cs
@@ -11,14 +11,23 @@ namespace DS4WinWPF.DS4Control.Profiles.Schema
     [AddINotifyPropertyChangedInterface]
     public class AutoSwitchingProfileEntry
     {
-        //public Dictionary<int, Guid> ControllerSlotProfileId { get; set; } = new(Enumerable
-        //    .Range(0, 8)
-        //    .Select(i => new KeyValuePair<int, Guid>(i, )));
+        /// <summary>
+        ///     Association between controller slots and the profile (ID) to switch to.
+        /// </summary>
+        public Dictionary<int, Guid?> ControllerSlotProfileId { get; set; } = new(Enumerable
+            .Range(0, 8)
+            .Select(i => new KeyValuePair<int, Guid?>(i, null)));
 
         public bool TurnOff { get; set; }
 
+        /// <summary>
+        ///     Full path to main executable file.
+        /// </summary>
         public string Path { get; set; }
 
+        /// <summary>
+        ///     Optional Window Title to further clamp down application detection.
+        /// </summary>
         public string Title { get; set; }
     }
 
--- DS4Windows/DS4Control/Profiles/Schema/ProfileSchema.cs
+++ DS4Windows/DS4Control/Profiles/Schema/ProfileSchema.cs
@@ -45,10 +45,12 @@ namespace DS4WinWPF.DS4Control.Profiles.Schema
         /// </summary>
         public static readonly Guid DefaultProfileId = Guid.Parse("C74D58EA-058F-4D01-BF08-8D765CC145D1");
         
+        /*
         /// <summary>
         ///     The <see cref="Guid"/> identifying a profile that indicates "no change" (for auto switching profiles).
         /// </summary>
         public static readonly Guid EmptyProfileId = Guid.Parse("D1D90BED-9D50-41D1-BAA7-049823FDBC25");
+        */
 
         public delegate void ProfilePropertyChangedEventHandler([CanBeNull] object sender,
             ProfilePropertyChangedEventArgs e);
@@ -91,11 +93,13 @@ namespace DS4WinWPF.DS4Control.Profiles.Schema
         [XmlIgnore]
         public bool IsDefaultProfile => Equals(Id, DefaultProfileId);
 
+        /*
         /// <summary>
         ///     If true, is an empty profile and should not be switched to.
         /// </summary>
         [XmlIgnore] 
         public bool IsEmptyProfile => Equals(Id, EmptyProfileId);
+        */
 
         /// <summary>
         ///     If true, this profile is linked to the current slots device' MAC/ID. This value is assigned at runtime and not
