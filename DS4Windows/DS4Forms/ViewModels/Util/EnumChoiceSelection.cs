94f1c733f09e96da827d6cbc8098b6351ec72152
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2021-12-19 16:14:48 +0100

    Refactored EnumChoiceSelection

--- DS4Windows/DS4Forms/ViewModels/Util/EnumChoiceSelection.cs
+++ DS4Windows/DS4Forms/ViewModels/Util/EnumChoiceSelection.cs
@@ -1,23 +1,15 @@
-﻿using System;
-
-namespace DS4WinWPF.DS4Forms.ViewModels.Util
+﻿namespace DS4WinWPF.DS4Forms.ViewModels.Util
 {
     public class EnumChoiceSelection<T>
     {
-        private string displayName;
-        public string DisplayName { get => displayName; }
-
-        private T choiceValue;
-        public T ChoiceValue
-        {
-            get => choiceValue;
-            set => choiceValue = value;
-        }
-
         public EnumChoiceSelection(string name, T currentValue)
         {
-            displayName = name;
-            choiceValue = currentValue;
+            DisplayName = name;
+            ChoiceValue = currentValue;
         }
+
+        public string DisplayName { get; }
+
+        public T ChoiceValue { get; set; }
     }
 }
