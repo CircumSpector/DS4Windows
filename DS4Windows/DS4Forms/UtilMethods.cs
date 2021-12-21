9a392b949e406c302211ba47c933f6c32b1d60f5
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2021-12-21 17:54:01 +0100

    Refactored UtilMethods

--- DS4Windows/DS4Forms/UtilMethods.cs
+++ DS4Windows/DS4Forms/UtilMethods.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Media;
 
@@ -14,30 +10,19 @@ namespace DS4WinWPF.DS4Forms
             where T : DependencyObject
         {
             if (depObj != null)
-            {
-                for (int i = 0; i < VisualTreeHelper.GetChildrenCount(depObj); i++)
+                for (var i = 0; i < VisualTreeHelper.GetChildrenCount(depObj); i++)
                 {
-                    DependencyObject child = VisualTreeHelper.GetChild(depObj, i);
-                    if (child != null && child is T)
-                    {
-                        yield return (T)child;
-                    }
+                    var child = VisualTreeHelper.GetChild(depObj, i);
+                    if (child != null && child is T) yield return (T)child;
 
-                    foreach (T childOfChild in FindVisualChildren<T>(child))
-                    {
-                        yield return childOfChild;
-                    }
+                    foreach (var childOfChild in FindVisualChildren<T>(child)) yield return childOfChild;
                 }
-            }
         }
 
         public static childItem FindVisualChild<childItem>(DependencyObject obj)
             where childItem : DependencyObject
         {
-            foreach (childItem child in FindVisualChildren<childItem>(obj))
-            {
-                return child;
-            }
+            foreach (var child in FindVisualChildren<childItem>(obj)) return child;
 
             return null;
         }
