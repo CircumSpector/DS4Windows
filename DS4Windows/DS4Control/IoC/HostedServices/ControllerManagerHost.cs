17d823cc7431fb7b77a882305c64435ff8925c13
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2022-01-17 11:23:02 +0100

    Minor change

--- DS4Windows/DS4Control/IoC/HostedServices/ControllerManagerHost.cs
+++ DS4Windows/DS4Control/IoC/HostedServices/ControllerManagerHost.cs
@@ -6,7 +6,7 @@ using Microsoft.Extensions.Hosting;
 
 namespace DS4WinWPF.DS4Control.IoC.HostedServices
 {
-    class ControllerManagerHost : BackgroundService
+    internal class ControllerManagerHost : BackgroundService
     {
         private readonly IControllerEnumeratorService controllerEnumeratorService;
 
