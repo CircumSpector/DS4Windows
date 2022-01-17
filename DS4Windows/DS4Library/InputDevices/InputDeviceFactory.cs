f02d5dcbf0784698820457fd506f315b2ced69f3
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2022-01-17 12:03:05 +0100

    Added comments Updated dependency

--- DS4Windows/DS4Library/InputDevices/InputDeviceFactory.cs
+++ DS4Windows/DS4Library/InputDevices/InputDeviceFactory.cs
@@ -6,10 +6,25 @@ namespace DS4WinWPF.DS4Library.InputDevices
 {
     public enum InputDeviceType : uint
     {
+        /// <summary>
+        ///     DualShock 4 variants
+        /// </summary>
         DualShock4,
+        /// <summary>
+        ///     Switch Pro Controller
+        /// </summary>
         SwitchPro,
+        /// <summary>
+        ///     Left JoyCons Controller
+        /// </summary>
         JoyConL,
+        /// <summary>
+        ///      Right JoyCons Controller
+        /// </summary>
         JoyConR,
+        /// <summary>
+        ///     DualSense Controller
+        /// </summary>
         DualSense
     }
 
