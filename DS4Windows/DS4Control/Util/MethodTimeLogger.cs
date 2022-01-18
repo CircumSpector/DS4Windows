3c5c1d3e8817144f1c89e64121b601f2a5579361
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2022-01-18 04:03:17 +0100

    Fixed verbose log message

--- DS4Windows/DS4Control/Util/MethodTimeLogger.cs
+++ DS4Windows/DS4Control/Util/MethodTimeLogger.cs
@@ -10,7 +10,7 @@ namespace DS4WinWPF.DS4Control.Util
         public static void Log(MethodBase methodBase, long milliseconds, string message)
         {
             Serilog.Log.Logger.Verbose("{Class}.{Method} took {Span}ms",
-                methodBase.ReflectedType?.Name, methodBase.ToString(), milliseconds);
+                methodBase.ReflectedType?.Name, methodBase.Name, milliseconds);
         }
     }
 }
