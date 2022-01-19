7f96cb20b55894f5a845898bb9c054ca5ef39d6c
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2022-01-19 19:20:31 +0100

    Added buffer stuff

--- DS4Windows.Shared.Core/HID/Devices/DualShock4CompatibleHidDevice.cs
+++ DS4Windows.Shared.Core/HID/Devices/DualShock4CompatibleHidDevice.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 using Microsoft.Extensions.Logging;
 
 namespace DS4Windows.Shared.Core.HID.Devices
@@ -6,17 +7,36 @@ namespace DS4Windows.Shared.Core.HID.Devices
     public class DualShock4CompatibleHidDevice : CompatibleHidDevice
     {
         private const byte SerialFeatureId = 18;
+        private const int UsbInputReportSize = 64;
+        private const int BthInputReportSize = 547;
 
         public DualShock4CompatibleHidDevice(InputDeviceType deviceType, HidDevice source,
             CompatibleHidDeviceFeatureSet featureSet, IServiceProvider serviceProvider) : base(deviceType, source,
             featureSet, serviceProvider)
         {
-            Serial = ReadSerial(SerialFeatureId);
+            Serial = Connection == ConnectionType.SonyWirelessAdapter
+                ? GenerateFakeHwSerial()
+                : ReadSerial(SerialFeatureId);
 
             if (Serial is null)
                 throw new ArgumentException("Could not retrieve a valid serial number.");
 
             Logger.LogInformation("Got serial {Serial} for {Device}", Serial, this);
+
+            if (Connection is ConnectionType.Usb or ConnectionType.SonyWirelessAdapter)
+            {
+                InputReportArray = new byte[UsbInputReportSize];
+                InputReportBuffer = Marshal.AllocHGlobal(InputReportArray.Length);
+
+                //
+                // TODO: finish me
+                // 
+            }
+            else
+            {
+                InputReportArray = new byte[BthInputReportSize];
+                InputReportBuffer = Marshal.AllocHGlobal(InputReportArray.Length);
+            }
         }
     }
 }
