b61a9337afe6221372e58aef0ab733830edc82e4
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2022-01-28 20:44:05 +0100

    Refined class

--- DS4Windows.Shared.Devices/HID/VidPidInfo.cs
+++ DS4Windows.Shared.Devices/HID/VidPidInfo.cs
@@ -1,28 +1,29 @@
 ﻿using System;
+using System.ComponentModel;
 
 namespace DS4Windows.Shared.Devices.HID
 {
-    // VidPidFeatureSet feature bit-flags (the default in VidPidInfo is zero value = standard DS4 behavior):
-    //
-    // DefaultDS4 (zero value) = Standard DS4 compatible communication (as it has been in DS4Win app for years)
-    // OnlyInputData0x01    = The incoming HID report data structure does NOT send 0x11 packet even in DS4 mode over BT connection. If this flag is set then accept "PC-friendly" 0x01 HID report data in BT just like how DS4 behaves in USB mode.
-    // OnlyOutputData0x05   = Outgoing HID report write data structure does NOT support DS4 BT 0x11 data structure. Use only "USB type of" 0x05 data packets even in BT connections.
-    // NoOutputData         = Gamepad doesn't support lightbar and rumble data writing at all. DS4Win app does not try to write out anything to gamepad.
-    // NoBatteryReading     = Gamepad doesn't send battery readings in the same format than DS4 gamepad (DS4Win app reports always 0% and starts to blink lightbar). Skip reading a battery fields and report fixed 99% battery level to avoid "low battery" LED flashes.
-    // NoGyroCalib          = Gamepad doesn't support or need gyro calibration routines. Skip gyro calibration if this flag is set. Some gamepad do have gyro, but don't support calibration or gyro sensors are missing.
-    // MonitorAudio         = Attempt to read volume levels for Gamepad headphone jack sink in Windows. Only with USB or SONYWA connections
-    // VendorDefinedDevice  = Accept the gamepad VID/PID even when it would be shown as vendor defined HID device on Windows (fex DS3 over DsMiniHid gamepad may have vendor defined HID type)
-    //
+    /// <summary>
+    ///     VidPidFeatureSet feature bit-flags (the default in VidPidInfo is zero value = standard DS4 behavior)
+    /// </summary>
     [Flags]
     public enum CompatibleHidDeviceFeatureSet : ushort
     {
+        [Description("Standard DS4 compatible communication (as it has been in DS4Win app for years)")]
         Default = 0,
+        [Description("The incoming HID report data structure does NOT send 0x11 packet even in DS4 mode over BT connection. If this flag is set then accept \"PC-friendly\" 0x01 HID report data in BT just like how DS4 behaves in USB mode.")]
         OnlyInputData0x01 = 1 << 0,
+        [Description("Outgoing HID report write data structure does NOT support DS4 BT 0x11 data structure. Use only \"USB type of\" 0x05 data packets even in BT connections.")]
         OnlyOutputData0x05 = 1 << 2,
+        [Description("Gamepad doesn't support lightbar and rumble data writing at all. DS4Win app does not try to write out anything to gamepad.")]
         NoOutputData = 1 << 3,
+        [Description("Gamepad doesn't send battery readings in the same format than DS4 gamepad (DS4Win app reports always 0% and starts to blink lightbar). Skip reading a battery fields and report fixed 99% battery level to avoid \"low battery\" LED flashes.")]
         NoBatteryReading = 1 << 4,
+        [Description("Gamepad doesn't support or need gyro calibration routines. Skip gyro calibration if this flag is set. Some gamepad do have gyro, but don't support calibration or gyro sensors are missing.")]
         NoGyroCalib = 1 << 5,
+        [Description("Attempt to read volume levels for Gamepad headphone jack sink in Windows. Only with USB or SONYWA connections")]
         MonitorAudio = 1 << 6,
+        [Description("Accept the gamepad VID/PID even when it would be shown as vendor defined HID device on Windows (fex DS3 over DsMiniHid gamepad may have vendor defined HID type)")]
         VendorDefinedDevice = 1 << 7
     }
 
@@ -43,14 +44,29 @@ namespace DS4Windows.Shared.Devices.HID
             FeatureSet = featureSet;
         }
 
+        /// <summary>
+        ///     The Vendor ID.
+        /// </summary>
         public int Vid { get; }
 
+        /// <summary>
+        ///     The Product ID.
+        /// </summary>
         public int Pid { get; }
 
+        /// <summary>
+        ///     The friendly display name.
+        /// </summary>
         public string Name { get; }
 
+        /// <summary>
+        ///     The <see cref="InputDeviceType"/>.
+        /// </summary>
         public InputDeviceType InputDevType { get; }
 
+        /// <summary>
+        ///     The <see cref="CompatibleHidDeviceFeatureSet"/>.
+        /// </summary>
         public CompatibleHidDeviceFeatureSet FeatureSet { get; }
     }
 }
