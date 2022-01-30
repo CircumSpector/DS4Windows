522f30fe83a75122d99f774aa2148273251cdf18
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2022-01-30 15:09:49 +0100

    Added docs

--- DS4Windows.Shared.Common/Types/BluetoothOutputReportMethod.cs
+++ DS4Windows.Shared.Common/Types/BluetoothOutputReportMethod.cs
@@ -1,6 +1,4 @@
-﻿using PInvoke;
-
-namespace DS4Windows.Shared.Common.Types
+﻿namespace DS4Windows.Shared.Common.Types
 {
     /// <summary>
     ///     Available Windows API to use to write to a wireless device.
@@ -8,11 +6,15 @@ namespace DS4Windows.Shared.Common.Types
     public enum BluetoothOutputReportMethod : uint
     {
         /// <summary>
-        ///     The WriteFile() function is used.
+        ///     The WriteFile() function is used. This should be preferred. Apparently there are issues with BLE and the Microsoft
+        ///     Bluetooth stack not working in some scenarios? It's hard to find actual information online, so for now just prefer
+        ///     this and experiment.
         /// </summary>
         WriteFile,
+
         /// <summary>
-        ///     The HidD_SetOutputReport() function is used.
+        ///     The HidD_SetOutputReport() function is used. This will invoke an IOCTL_HID_SET_OUTPUT_REPORT request and should be
+        ///     considered a blocking call until the request is answered by the remote hardware.
         /// </summary>
         HidD_SetOutputReport
     }
