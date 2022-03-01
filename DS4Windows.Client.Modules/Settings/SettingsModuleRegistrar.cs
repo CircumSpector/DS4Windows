012091a99b14cbb54c8944e17ee6c52f8ad9ba8e
Jason Rainwater <torinth@outlook.com>
2022-03-01 18:28:12 -0500

    added the settings module

---
+++ DS4Windows.Client.Modules/Settings/SettingsModuleRegistrar.cs
@@ -0,0 +1,16 @@
+﻿using DS4Windows.Client.Core.DependencyInjection;
+using DS4Windows.Client.Core.ViewModel;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DS4Windows.Client.Modules.Settings
+{
+    public class SettingsModuleRegistrar : IServiceRegistrar
+    {
+        public void ConfigureServices(IConfiguration configuration, IServiceCollection services)
+        {
+            services.AddSingletons<SettingsViewModel>(typeof(ISettingsViewModel), typeof(INavigationTabViewModel));
+            services.AddSingleton<ISettingsView, SettingsView>();
+        }
+    }
+}
