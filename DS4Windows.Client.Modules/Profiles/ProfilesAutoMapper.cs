c4e0c7a356cb5b09c1d582d2244a633adc8152ea
Jason Rainwater <torinth@outlook.com>
2022-03-22 15:06:34 -0400

    added more profile properties

--- DS4Windows.Client.Modules/Profiles/ProfilesAutoMapper.cs
+++ DS4Windows.Client.Modules/Profiles/ProfilesAutoMapper.cs
@@ -18,7 +18,9 @@ namespace DS4Windows.Client.Modules.Profiles
 
             CreateMap<IProfile, ProfileEditViewModel>()
                 .ForMember(dest => dest.Name, cfg => cfg.MapFrom(src => src.DisplayName))
+
             #region Left Stick
+
                 .ForPath(dest => dest.LeftStick.ControlModeSettings.DeadZone, cfg => cfg.MapFrom(src => src.LSModInfo.DeadZone))
                 .ForPath(dest => dest.LeftStick.ControlModeSettings.AntiDeadZone, cfg => cfg.MapFrom(src => src.LSModInfo.AntiDeadZone))
                 .ForPath(dest => dest.LeftStick.ControlModeSettings.MaxZone, cfg => cfg.MapFrom(src => src.LSModInfo.DeadZone))
@@ -26,9 +28,20 @@ namespace DS4Windows.Client.Modules.Profiles
                 .ForPath(dest => dest.LeftStick.ControlModeSettings.DeadZoneType, cfg => cfg.MapFrom(src => src.LSModInfo.DZType))
                 .ForPath(dest => dest.LeftStick.ControlModeSettings.VerticalScale, cfg => cfg.MapFrom(src => src.LSModInfo.VerticalScale))
                 .ForPath(dest => dest.LeftStick.ControlModeSettings.Sensitivity, cfg => cfg.MapFrom(src => src.LSSens))
+                .ForPath(dest => dest.LeftStick.ControlModeSettings.XDeadZone, cfg => cfg.MapFrom(src => src.LSModInfo.XAxisDeadInfo.DeadZone))
+                .ForPath(dest => dest.LeftStick.ControlModeSettings.XMaxZone, cfg => cfg.MapFrom(src => src.LSModInfo.XAxisDeadInfo.MaxZone))
+                .ForPath(dest => dest.LeftStick.ControlModeSettings.XAntiDeadZone, cfg => cfg.MapFrom(src => src.LSModInfo.XAxisDeadInfo.AntiDeadZone))
+                .ForPath(dest => dest.LeftStick.ControlModeSettings.XMaxOutput, cfg => cfg.MapFrom(src => src.LSModInfo.XAxisDeadInfo.MaxOutput))
+                .ForPath(dest => dest.LeftStick.ControlModeSettings.YDeadZone, cfg => cfg.MapFrom(src => src.LSModInfo.YAxisDeadInfo.DeadZone))
+                .ForPath(dest => dest.LeftStick.ControlModeSettings.YMaxZone, cfg => cfg.MapFrom(src => src.LSModInfo.YAxisDeadInfo.MaxZone))
+                .ForPath(dest => dest.LeftStick.ControlModeSettings.YAntiDeadZone, cfg => cfg.MapFrom(src => src.LSModInfo.YAxisDeadInfo.AntiDeadZone))
+                .ForPath(dest => dest.LeftStick.ControlModeSettings.YMaxOutput, cfg => cfg.MapFrom(src => src.LSModInfo.YAxisDeadInfo.MaxOutput))
                 .ForPath(dest => dest.LeftStick.OutputSettings, cfg => cfg.MapFrom(src => src.LSOutputSettings.Mode))
+                
             #endregion
+
             #region Right Stick
+
                 .ForPath(dest => dest.RightStick.ControlModeSettings.DeadZone, cfg => cfg.MapFrom(src => src.RSModInfo.DeadZone))
                 .ForPath(dest => dest.RightStick.ControlModeSettings.AntiDeadZone, cfg => cfg.MapFrom(src => src.RSModInfo.AntiDeadZone))
                 .ForPath(dest => dest.RightStick.ControlModeSettings.MaxZone, cfg => cfg.MapFrom(src => src.RSModInfo.DeadZone))
@@ -36,12 +49,23 @@ namespace DS4Windows.Client.Modules.Profiles
                 .ForPath(dest => dest.RightStick.ControlModeSettings.DeadZoneType, cfg => cfg.MapFrom(src => src.RSModInfo.DZType))
                 .ForPath(dest => dest.RightStick.ControlModeSettings.VerticalScale, cfg => cfg.MapFrom(src => src.RSModInfo.VerticalScale))
                 .ForPath(dest => dest.RightStick.ControlModeSettings.Sensitivity, cfg => cfg.MapFrom(src => src.RSSens))
+                .ForPath(dest => dest.RightStick.ControlModeSettings.XDeadZone, cfg => cfg.MapFrom(src => src.RSModInfo.XAxisDeadInfo.DeadZone))
+                .ForPath(dest => dest.RightStick.ControlModeSettings.XMaxZone, cfg => cfg.MapFrom(src => src.RSModInfo.XAxisDeadInfo.MaxZone))
+                .ForPath(dest => dest.RightStick.ControlModeSettings.XAntiDeadZone, cfg => cfg.MapFrom(src => src.RSModInfo.XAxisDeadInfo.AntiDeadZone))
+                .ForPath(dest => dest.RightStick.ControlModeSettings.XMaxOutput, cfg => cfg.MapFrom(src => src.RSModInfo.XAxisDeadInfo.MaxOutput))
+                .ForPath(dest => dest.RightStick.ControlModeSettings.YDeadZone, cfg => cfg.MapFrom(src => src.RSModInfo.YAxisDeadInfo.DeadZone))
+                .ForPath(dest => dest.RightStick.ControlModeSettings.YMaxZone, cfg => cfg.MapFrom(src => src.RSModInfo.YAxisDeadInfo.MaxZone))
+                .ForPath(dest => dest.RightStick.ControlModeSettings.YAntiDeadZone, cfg => cfg.MapFrom(src => src.RSModInfo.YAxisDeadInfo.AntiDeadZone))
+                .ForPath(dest => dest.RightStick.ControlModeSettings.YMaxOutput, cfg => cfg.MapFrom(src => src.RSModInfo.YAxisDeadInfo.MaxOutput))
                 .ForPath(dest => dest.RightStick.OutputSettings, cfg => cfg.MapFrom(src => src.RSOutputSettings.Mode));
+
             #endregion
 
             CreateMap<ProfileEditViewModel, IProfile>()
                 .ForMember(dest => dest.DisplayName, cfg => cfg.MapFrom(src => src.Name))
+
             #region Left Stick
+
                 .ForPath(dest => dest.LSModInfo.DeadZone, cfg => cfg.MapFrom(src => src.LeftStick.ControlModeSettings.DeadZone))
                 .ForPath(dest => dest.LSModInfo.AntiDeadZone, cfg => cfg.MapFrom(src => src.LeftStick.ControlModeSettings.AntiDeadZone))
                 .ForPath(dest => dest.LSModInfo.MaxZone, cfg => cfg.MapFrom(src => src.LeftStick.ControlModeSettings.DeadZone))
@@ -49,8 +73,18 @@ namespace DS4Windows.Client.Modules.Profiles
                 .ForPath(dest => dest.LSModInfo.DZType, cfg => cfg.MapFrom(src => src.LeftStick.ControlModeSettings.DeadZoneType))
                 .ForPath(dest => dest.LSModInfo.VerticalScale, cfg => cfg.MapFrom(src => src.LeftStick.ControlModeSettings.VerticalScale))
                 .ForPath(dest => dest.LSSens, cfg => cfg.MapFrom(src => src.LeftStick.ControlModeSettings.Sensitivity))
+                .ForPath(dest => dest.LSModInfo.XAxisDeadInfo.DeadZone, cfg => cfg.MapFrom(src => src.LeftStick.ControlModeSettings.XDeadZone))
+                .ForPath(dest => dest.LSModInfo.XAxisDeadInfo.MaxZone, cfg => cfg.MapFrom(src => src.LeftStick.ControlModeSettings.XMaxZone))
+                .ForPath(dest => dest.LSModInfo.XAxisDeadInfo.AntiDeadZone, cfg => cfg.MapFrom(src => src.LeftStick.ControlModeSettings.XAntiDeadZone))
+                .ForPath(dest => dest.LSModInfo.XAxisDeadInfo.MaxOutput, cfg => cfg.MapFrom(src => src.LeftStick.ControlModeSettings.XMaxOutput))
+                .ForPath(dest => dest.LSModInfo.YAxisDeadInfo.DeadZone, cfg => cfg.MapFrom(src => src.LeftStick.ControlModeSettings.YDeadZone))
+                .ForPath(dest => dest.LSModInfo.YAxisDeadInfo.MaxZone, cfg => cfg.MapFrom(src => src.LeftStick.ControlModeSettings.YMaxZone))
+                .ForPath(dest => dest.LSModInfo.YAxisDeadInfo.AntiDeadZone, cfg => cfg.MapFrom(src => src.LeftStick.ControlModeSettings.YAntiDeadZone))
+                .ForPath(dest => dest.LSModInfo.YAxisDeadInfo.MaxOutput, cfg => cfg.MapFrom(src => src.LeftStick.ControlModeSettings.YMaxOutput))
                 .ForPath(dest => dest.LSOutputSettings.Mode, cfg => cfg.MapFrom(src => src.LeftStick.OutputSettings))
+
             #endregion
+
             #region Right Stick
                 .ForPath(dest => dest.RSModInfo.DeadZone, cfg => cfg.MapFrom(src => src.RightStick.ControlModeSettings.DeadZone))
                 .ForPath(dest => dest.RSModInfo.AntiDeadZone, cfg => cfg.MapFrom(src => src.RightStick.ControlModeSettings.AntiDeadZone))
@@ -59,6 +93,14 @@ namespace DS4Windows.Client.Modules.Profiles
                 .ForPath(dest => dest.RSModInfo.DZType, cfg => cfg.MapFrom(src => src.RightStick.ControlModeSettings.DeadZoneType))
                 .ForPath(dest => dest.RSModInfo.VerticalScale, cfg => cfg.MapFrom(src => src.RightStick.ControlModeSettings.VerticalScale))
                 .ForPath(dest => dest.RSSens, cfg => cfg.MapFrom(src => src.RightStick.ControlModeSettings.Sensitivity))
+                .ForPath(dest => dest.RSModInfo.XAxisDeadInfo.DeadZone, cfg => cfg.MapFrom(src => src.RightStick.ControlModeSettings.XDeadZone))
+                .ForPath(dest => dest.RSModInfo.XAxisDeadInfo.MaxZone, cfg => cfg.MapFrom(src => src.RightStick.ControlModeSettings.XMaxZone))
+                .ForPath(dest => dest.RSModInfo.XAxisDeadInfo.AntiDeadZone, cfg => cfg.MapFrom(src => src.RightStick.ControlModeSettings.XAntiDeadZone))
+                .ForPath(dest => dest.RSModInfo.XAxisDeadInfo.MaxOutput, cfg => cfg.MapFrom(src => src.RightStick.ControlModeSettings.XMaxOutput))
+                .ForPath(dest => dest.RSModInfo.YAxisDeadInfo.DeadZone, cfg => cfg.MapFrom(src => src.RightStick.ControlModeSettings.YDeadZone))
+                .ForPath(dest => dest.RSModInfo.YAxisDeadInfo.MaxZone, cfg => cfg.MapFrom(src => src.RightStick.ControlModeSettings.YMaxZone))
+                .ForPath(dest => dest.RSModInfo.YAxisDeadInfo.AntiDeadZone, cfg => cfg.MapFrom(src => src.RightStick.ControlModeSettings.YAntiDeadZone))
+                .ForPath(dest => dest.RSModInfo.YAxisDeadInfo.MaxOutput, cfg => cfg.MapFrom(src => src.RightStick.ControlModeSettings.YMaxOutput))
                 .ForPath(dest => dest.RSOutputSettings.Mode, cfg => cfg.MapFrom(src => src.RightStick.OutputSettings));
             #endregion
         }
