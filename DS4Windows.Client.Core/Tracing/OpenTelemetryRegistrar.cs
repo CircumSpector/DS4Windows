53822585610596ebffb8d2ddc351731bb27cad3f
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2022-04-23 17:29:52 +0200

    Finished OpenTelemetry registration

--- DS4Windows.Client.Core/Tracing/OpenTelemetryRegistrar.cs
+++ DS4Windows.Client.Core/Tracing/OpenTelemetryRegistrar.cs
@@ -1,35 +1,50 @@
-﻿using DS4Windows.Client.Core.DependencyInjection;
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using DS4Windows.Client.Core.DependencyInjection;
+using JetBrains.Annotations;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System;
+using OpenTelemetry;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 
-namespace DS4Windows.Client.Core.Tracing
+namespace DS4Windows.Client.Core.Tracing;
+
+[UsedImplicitly]
+public class OpenTelemetryRegistrar : IServiceRegistrar
 {
-    public class OpenTelemetryRegistrar : IServiceRegistrar
-    {
-        public void ConfigureServices(IConfiguration configuration, IServiceCollection services)
-        {
-            /*if (bool.TryParse(configuration.GetSection("OpenTelemetry:IsTracingEnabled").Value, out var isEnabled) &&
-                isEnabled)
-                //
-                // Initialize OpenTelemetry
-                // 
-                //TODO: open telemetry keeps giving me problems, need to figure it out and fix it later
-                services.AddOpenTelemetryTracing(builder => builder
-                    .SetSampler(new AlwaysOnSampler())
-                    .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(Constants.ApplicationName))
-                    .AddSource(Constants.ApplicationName)
-                    .AddSource(TracingSources.DevicesAssemblyActivitySourceName)
-                    .AddSource(TracingSources.CommonAssemblyActivitySourceName)
-                    .AddSource(TracingSources.ConfigurationApplicationAssemblyActivitySourceName)
-                    .AddJaegerExporter(options => { options.ExportProcessorType = ExportProcessorType.Simple; })
-            
-                );
-            */
-        }
+    private const string AssemblyPrefix = "DS4Windows";
 
-        public void Initialize(IServiceProvider services)
-        {
-        }
+    private static readonly Regex Cleanup = new(", (Version|Culture|PublicKeyToken)=[0-9.\\w]+", RegexOptions.Compiled);
+
+    public void ConfigureServices(IConfiguration configuration, IServiceCollection services)
+    {
+        // Get list of assemblies, register them all as potential tracing sources
+        var assemblyNames = AppDomain.CurrentDomain
+            .GetAssemblies()
+            .Where(assembly =>
+            {
+                var name = assembly.GetName().Name;
+                return name != null && name.StartsWith(AssemblyPrefix);
+            })
+            .Select(assembly => Cleanup.Replace(assembly.GetName().Name!, string.Empty))
+            .ToArray();
+
+        if (bool.TryParse(configuration.GetSection("OpenTelemetry:IsTracingEnabled").Value, out var isEnabled) &&
+            isEnabled)
+            //
+            // Initialize OpenTelemetry
+            // 
+            services.AddOpenTelemetryTracing(builder => builder
+                .SetSampler(new AlwaysOnSampler())
+                .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(AssemblyPrefix))
+                .AddSource(assemblyNames)
+                .AddJaegerExporter(options => { options.ExportProcessorType = ExportProcessorType.Simple; })
+            );
+    }
+
+    public void Initialize(IServiceProvider services)
+    {
     }
 }
