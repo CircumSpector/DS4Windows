53ac992b790bf104b7d510408a1d1938be5c2322
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2022-09-20 17:30:54 +0200

    Code clean-up

--- DS4Windows.Server.Interfaces/Controller/ControllerConnectedMessage.cs
+++ DS4Windows.Server.Interfaces/Controller/ControllerConnectedMessage.cs
@@ -1,22 +1,32 @@
 ﻿using Ds4Windows.Shared.Devices.Interfaces.HID;
 
-namespace DS4Windows.Server.Controller
-{
-    public class ControllerConnectedMessage : MessageBase
-    {
-        public const string Name = "ControllerConnected";
+namespace DS4Windows.Server.Controller;
 
-        public string Description { get; set; }
-        public string DisplayName { get; set; }
-        public string InstanceId { get; set; }
-        public string ManufacturerString { get; set; }
-        public string ParentInstance { get; set; }
-        public string Path { get; set; }
-        public string ProductString { get; set; }
-        public string SerialNumberString { get; set; }
-        public ConnectionType Connection { get; set; }
-        public InputDeviceType DeviceType { get; set; }
-        public Guid SelectedProfileId { get; set; }
-        public override string MessageName => Name;
-    }
+public class ControllerConnectedMessage : MessageBase
+{
+    public const string Name = "ControllerConnected";
+
+    public string Description { get; set; }
+
+    public string DisplayName { get; set; }
+
+    public string InstanceId { get; init; }
+
+    public string ManufacturerString { get; set; }
+
+    public string ParentInstance { get; init; }
+
+    public string Path { get; set; }
+
+    public string ProductString { get; set; }
+
+    public string SerialNumberString { get; init; }
+
+    public ConnectionType Connection { get; init; }
+
+    public InputDeviceType DeviceType { get; init; }
+    
+    public Guid SelectedProfileId { get; init; }
+
+    public override string MessageName => Name;
 }
--- DS4Windows.Server.Interfaces/MessageBase.cs
+++ DS4Windows.Server.Interfaces/MessageBase.cs
@@ -1,13 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace DS4Windows.Server;
 
-namespace DS4Windows.Server
+public class MessageBase
 {
-    public class MessageBase
-    {
-        public virtual string MessageName { get; set; }
-    }
+    public virtual string MessageName { get; set; }
 }
