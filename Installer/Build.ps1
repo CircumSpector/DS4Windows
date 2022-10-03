812673afeebe06e91dde0726a453c9642f39805a
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2022-10-03 21:38:58 +0200

    Added some logging to build script

--- Installer/Build.ps1
+++ Installer/Build.ps1
@@ -9,4 +9,8 @@ if (Test-Path -Path $publishFolder)
 	Remove-Item -Path $publishFolder -Recurse
 }
 
+Write-Host "Starting dotnet publish" -ForegroundColor Green
+
 dotnet publish ($env:APPVEYOR_BUILD_FOLDER + "/Vapour.sln") /p:PlatformTarget=$env:PLATFORM /p:PublishProfile=release-win-$env:PLATFORM
+
+Write-Host "Finished dotnet publish" -ForegroundColor Green
