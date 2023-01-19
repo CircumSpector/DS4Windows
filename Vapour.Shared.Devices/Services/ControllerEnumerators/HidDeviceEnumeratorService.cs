76e40548b511882b81690654bd9b4ab46c59b853
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2023-01-19 19:28:27 +0100

    Added HidDeviceEnumeratorException class

--- Vapour.Shared.Devices/Services/ControllerEnumerators/HidDeviceEnumeratorService.cs
+++ Vapour.Shared.Devices/Services/ControllerEnumerators/HidDeviceEnumeratorService.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
 using System.Runtime.InteropServices;
 
 using Windows.Win32;
@@ -17,8 +18,22 @@ using Vapour.Shared.Devices.HID;
 namespace Vapour.Shared.Devices.Services.ControllerEnumerators;
 
 /// <summary>
+///     Potential exception thrown by <see cref="HidDeviceEnumeratorService"/>.
+/// </summary>
+[SuppressMessage("ReSharper", "UnusedMember.Global")]
+public sealed class HidDeviceEnumeratorException : Exception
+{
+    internal HidDeviceEnumeratorException(string message) : base(message)
+    {
+    }
+
+    public int NativeError { get; } = Marshal.GetLastWin32Error();
+}
+
+/// <summary>
 ///     Single point of truth of states for all connected and handled HID devices.
 /// </summary>
+[SuppressMessage("ReSharper", "MemberCanBePrivate.Global")]
 internal sealed class HidDeviceEnumeratorService : IHidDeviceEnumeratorService<HidDevice>
 {
     private readonly ActivitySource _coreActivity = new(TracingSources.AssemblyName);
@@ -53,6 +68,7 @@ internal sealed class HidDeviceEnumeratorService : IHidDeviceEnumeratorService<H
     /// <inheritdoc />
     public event Action<HidDevice> DeviceArrived;
 
+    /// <inheritdoc />
     public event Action<string> DeviceRemoved;
 
     /// <inheritdoc />
@@ -62,7 +78,7 @@ internal sealed class HidDeviceEnumeratorService : IHidDeviceEnumeratorService<H
             $"{nameof(HidDeviceEnumeratorService)}:{nameof(Start)}");
 
         int deviceIndex = 0;
-        
+
         while (Devcon.FindByInterfaceGuid(_hidClassInterfaceGuid, out string path, out _, deviceIndex++))
         {
             try
@@ -107,7 +123,7 @@ internal sealed class HidDeviceEnumeratorService : IHidDeviceEnumeratorService<H
     {
         RemoveDevice(args.SymLink);
     }
-    
+
     private void CreateNewHidDevice(string path)
     {
         using Activity activity = _coreActivity.StartActivity(
@@ -155,7 +171,7 @@ internal sealed class HidDeviceEnumeratorService : IHidDeviceEnumeratorService<H
 
         GetHidCapabilities(path, out HIDP_CAPS caps);
 
-        var hidDevice = new HidDevice
+        HidDevice hidDevice = new HidDevice
         {
             Path = path,
             InstanceId = device.InstanceId.ToUpper(),
@@ -181,15 +197,15 @@ internal sealed class HidDeviceEnumeratorService : IHidDeviceEnumeratorService<H
 
         activity?.SetTag("Path", symLink);
 
-        PnPDevice device = PnPDevice.GetDeviceByInterfaceId(symLink, DeviceLocationFlags.Phantom); 
+        PnPDevice device = PnPDevice.GetDeviceByInterfaceId(symLink, DeviceLocationFlags.Phantom);
         _logger.LogInformation("HID Device ({InstanceId} ({SymLink}) removed", device.InstanceId, symLink);
-        
+
         if (!device.IsVirtual())
         {
             DeviceRemoved?.Invoke(device.InstanceId);
         }
     }
-    
+
     /// <summary>
     ///     Attempts to open device.
     /// </summary>
@@ -213,7 +229,7 @@ internal sealed class HidDeviceEnumeratorService : IHidDeviceEnumeratorService<H
             true when Marshal.GetLastWin32Error() == (int)WIN32_ERROR.ERROR_ACCESS_DENIED => false,
             true when Marshal.GetLastWin32Error() == (int)WIN32_ERROR.ERROR_SHARING_VIOLATION => false,
             true when Marshal.GetLastWin32Error() == (int)WIN32_ERROR.ERROR_FILE_NOT_FOUND => false,
-            true => throw new Exception($"Couldn't open device handle, error {Marshal.GetLastWin32Error()}"),
+            true => throw new HidDeviceEnumeratorException("Couldn't open device handle."),
             _ => true
         };
     }
@@ -349,12 +365,12 @@ internal sealed class HidDeviceEnumeratorService : IHidDeviceEnumeratorService<H
 
         if (handle.IsInvalid)
         {
-            throw new Exception($"Couldn't open device handle, error {Marshal.GetLastWin32Error()}");
+            throw new HidDeviceEnumeratorException("Couldn't open device handle.");
         }
 
         if (!PInvoke.HidD_GetPreparsedData(handle, out nint dataHandle))
         {
-            throw new Exception($"HidD_GetPreparsedData failed with error {Marshal.GetLastWin32Error()}");
+            throw new HidDeviceEnumeratorException("HidD_GetPreparsedData failed.");
         }
 
         PInvoke.HidP_GetCaps(dataHandle, out caps);
