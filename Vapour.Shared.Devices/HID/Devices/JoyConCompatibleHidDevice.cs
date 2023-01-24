ec5fd9c28ff4f08c83685f102ca45a8705137f9e
Torinth <torinth@outlook.com>
2023-01-24 11:52:56 -0600

    implemented stick support

--- Vapour.Shared.Devices/HID/Devices/JoyConCompatibleHidDevice.cs
+++ Vapour.Shared.Devices/HID/Devices/JoyConCompatibleHidDevice.cs
@@ -1,6 +1,4 @@
-﻿using System.Net.NetworkInformation;
-
-using Microsoft.Extensions.Logging;
+﻿using Microsoft.Extensions.Logging;
 
 using Vapour.Shared.Devices.HID.DeviceInfos;
 using Vapour.Shared.Devices.HID.Devices.Reports;
@@ -9,21 +7,136 @@ namespace Vapour.Shared.Devices.HID.Devices;
 
 public sealed class JoyConCompatibleHidDevice : CompatibleHidDevice
 {
+    private const int SubCommandHeaderLength = 8;
+    private const int SubCommandLength = 64;
+    private static readonly byte[] _subCommandHeader =
+        { 0x0, 0x1, 0x40, 0x40, 0x0, 0x1, 0x40, 0x40 };
+    private byte _commandCount;
+
+    private const int SpiDataOffset = 20;
+
+    private readonly JoyConCompatibleInputReport _report = new();
+
     public JoyConCompatibleHidDevice(ILogger<JoyConCompatibleHidDevice> logger, List<DeviceInfo> deviceInfos)
         : base(logger, deviceInfos)
     {
     }
 
+    public bool IsLeft
+    {
+        get
+        {
+            return CurrentDeviceInfo != null && CurrentDeviceInfo.GetType() == typeof(JoyConLeftDeviceInfo);
+        }
+    }
+
     protected override void OnInitialize()
     {
-        Serial = PhysicalAddress.Parse(SourceDevice.SerialNumberString);
+        _report.IsLeft = IsLeft;
+        Serial = ReadSerial(JoyConCodes.FeatureId_Serial);
+        SubCommand(JoyConCodes.SubCommand_InputMode, JoyConCodes.InputMode_SimpleHid);
+        SubCommand(JoyConCodes.SubCommand_EnableIMU, JoyConCodes.EnableIMU_On);
+        SubCommand(JoyConCodes.SubCommand_SetPlayerLED, JoyConCodes.SetPlayerLED1);
+
+        GetCalibrationData();
+
+        SubCommand(JoyConCodes.SubCommand_InputMode, JoyConCodes.InputMode_Standard);
     }
 
     public override void ProcessInputReport(ReadOnlySpan<byte> input)
     {
+        InputSourceReport.Parse(input);
     }
 
-    public override InputSourceReport InputSourceReport { get; } = new JoyConCompatibleInputReport();
+    public override InputSourceReport InputSourceReport => _report;
 
     protected override InputDeviceType InputDeviceType => InputDeviceType.JoyCon;
+
+    private void GetCalibrationData()
+    {
+        var userCalibrationFound = false;
+        var resultData = SubCommand(JoyConCodes.SubCommand_SpiFlashRead, IsLeft ? JoyConCodes.GetLeftStickUserCalibration : JoyConCodes.GetRightStickUserCalibration);
+        var spiData = resultData.Slice(SpiDataOffset, 9);
+
+        for (int i = 0; i < 9; ++i)
+        {
+            if (spiData[i] != 0xff)
+            {
+                userCalibrationFound = true;
+                break;
+            }
+        }
+
+        if (!userCalibrationFound)
+        {
+            resultData = SubCommand(JoyConCodes.SubCommand_SpiFlashRead, IsLeft ? JoyConCodes.GetLeftStickFactoryCalibration : JoyConCodes.GetRightStickFactoryCalibration);
+            spiData = resultData.Slice(SpiDataOffset, 9);
+        }
+        
+        _report.StickCalibration[IsLeft ? 0 : 2] = (ushort)((spiData[1] << 8) & 0xF00 | spiData[0]); // X Axis Max above center
+        _report.StickCalibration[IsLeft ? 1 : 3] = (ushort)((spiData[2] << 4) | (spiData[1] >> 4));  // Y Axis Max above center
+        _report.StickCalibration[IsLeft ? 2 : 4] = (ushort)((spiData[4] << 8) & 0xF00 | spiData[3]); // X Axis Center
+        _report.StickCalibration[IsLeft ? 3 : 5] = (ushort)((spiData[5] << 4) | (spiData[4] >> 4));  // Y Axis Center
+        _report.StickCalibration[IsLeft ? 4 : 0] = (ushort)((spiData[7] << 8) & 0xF00 | spiData[6]); // X Axis Min below center
+        _report.StickCalibration[IsLeft ? 5 : 1] = (ushort)((spiData[8] << 4) | (spiData[7] >> 4));  // Y Axis Min below center
+
+        resultData = SubCommand(JoyConCodes.SubCommand_SpiFlashRead, JoyConCodes.GetStickParameters);
+        spiData = resultData.Slice(SpiDataOffset, 16);
+        _report.DeadZone = (ushort)((spiData[4] << 8) & 0xF00 | spiData[3]);
+    }
+
+    private ReadOnlySpan<byte> SubCommand(byte subCommand, byte[] data)
+    {
+        byte[] commandData = new byte[SubCommandLength];
+        Array.Copy(_subCommandHeader, 0, commandData, 2, SubCommandHeaderLength);
+        Array.Copy(data, 0, commandData, 11, data.Length);
+
+        commandData[0] = 0x01;
+        commandData[1] = _commandCount;
+        _commandCount = (byte)(++_commandCount & 0x0F);
+        commandData[10] = subCommand;
+
+        var writeResult = SourceDevice.WriteOutputReportViaInterrupt(commandData, 1000);
+
+        var resultData = new byte[362];
+        if (writeResult)
+        {
+            int bytesRead;
+            int retryCount = 0;
+            do
+            {
+                bytesRead = SourceDevice.ReadInputReport(resultData);
+                retryCount++;
+            } while (bytesRead > 0 && resultData[0] != 0x21 && resultData[14] != subCommand && retryCount < 100);
+        }
+
+        return resultData;
+    }
+
+    private static class JoyConCodes
+    {
+        public const byte SubCommand_InputMode = 0x03;
+        public const byte SubCommand_EnableIMU = 0x40;
+        public const byte SubCommand_SetPlayerLED = 0x30;
+        public const byte SubCommand_SpiFlashRead = 0x10;
+
+        public static readonly byte[] InputMode_Standard = { 0x30 };
+        public static readonly byte[] InputMode_SimpleHid = { 0x3F };
+
+        public const byte FeatureId_Serial = 18;
+
+        public static readonly byte[] EnableIMU_On = { 0x01 };
+
+        public static readonly byte[] SetPlayerLED1 = { 0x01 | 0x01 };
+        public static readonly byte[] SetPlayerLED2 = { 0x01 | 0x02 };
+        public static readonly byte[] SetPlayerLED3 = { 0x01 | 0x02 | 0x04 };
+        public static readonly byte[] SetPlayerLED4 = { 0x01 | 0x02 | 0x04 | 0x08 };
+
+        public static readonly byte[] GetLeftStickUserCalibration = { 0x12, 0x80, 0x00, 0x00, 0x09 };
+        public static readonly byte[] GetLeftStickFactoryCalibration = { 0x3D, 0x60, 0x00, 0x00, 0x09 };
+        public static readonly byte[] GetStickParameters = { 0x86, 0x60, 0x00, 0x00, 0x12 };
+
+        public static readonly byte[] GetRightStickUserCalibration = { 0x1D, 0x80, 0x00, 0x00, 0x09 };
+        public static readonly byte[] GetRightStickFactoryCalibration = { 0x46, 0x60, 0x00, 0x00, 0x09 };
+    }
 }
--- Vapour.Shared.Devices/HID/Devices/Reports/JoyConCompatibleInputReport.cs
+++ Vapour.Shared.Devices/HID/Devices/Reports/JoyConCompatibleInputReport.cs
@@ -2,122 +2,59 @@
 
 namespace Vapour.Shared.Devices.HID.Devices.Reports;
 
-/// <summary>
-///     TODO: unfinished! Currently just copied over from DualShock
-/// </summary>
 public sealed class JoyConCompatibleInputReport : InputSourceReport
 {
-    public override InputAxisType AxisScaleInputType => InputAxisType.DualShock4;
+    private readonly float[] _tempStickData = new float[2];
+    private readonly short[] _finalStickData = new short[2];
 
-    public TrackPadTouch TrackPadTouch1 { get; set; }
-
-    public TrackPadTouch TrackPadTouch2 { get; set; }
-
-    public byte TouchPacketCounter { get; set; }
-
-    public bool TouchOneFingerActive => Touch1 || Touch2;
-
-    public bool TouchTwoFingersActive => Touch1 && Touch2;
-
-    public bool Mute { get; set; }
-
-    /// <summary>
-    ///     First (one finger) touch is registered.
-    /// </summary>
-    public bool Touch1 { get; set; }
-
-    /// <summary>
-    ///     Second (two fingers) touch is registered.
-    /// </summary>
-    public bool Touch2 { get; set; }
-
-    public bool TouchIsOnLeftSide { get; set; }
-
-    public bool TouchIsOnRightSide { get; set; }
-
-    public bool TouchClick { get; set; }
+    public override InputAxisType AxisScaleInputType => InputAxisType.Xbox;
+    
+    public ushort[] StickCalibration { get; } = new ushort[6];
+    public ushort DeadZone { get; set; }
+    public bool IsLeft { get; set; }
 
     /// <inheritdoc />
     public override void Parse(ReadOnlySpan<byte> input)
     {
-        // Eliminate bounds checks
-        input = input.Slice(0, 43);
-
-        ReportId = input[0];
-
-        LeftThumbX = input[1];
-        LeftThumbY = input[2];
-        RightThumbX = input[3];
-        RightThumbY = input[4];
-        LeftTrigger = input[8];
-        RightTrigger = input[9];
-
-        Triangle = (input[5] & (1 << 7)) != 0;
-        Circle = (input[5] & (1 << 6)) != 0;
-        Cross = (input[5] & (1 << 5)) != 0;
-        Square = (input[5] & (1 << 4)) != 0;
-
-        DPad = (DPadDirection)(input[5] & 0x0F);
-
-        LeftThumb = (input[6] & (1 << 6)) != 0;
-        RightThumb = (input[6] & (1 << 7)) != 0;
-        Options = (input[6] & (1 << 5)) != 0;
-        Share = (input[6] & (1 << 4)) != 0;
-        RightTriggerButton = (input[6] & (1 << 3)) != 0;
-        LeftTriggerButton = (input[6] & (1 << 2)) != 0;
-        RightShoulder = (input[6] & (1 << 1)) != 0;
-        LeftShoulder = (input[6] & (1 << 0)) != 0;
-
-        PS = (input[7] & (1 << 0)) != 0;
-        TouchClick = (input[7] & (1 << 1)) != 0;
-
-        FrameCounter = (byte)(input[7] >> 2);
-
-        var touchX = ((input[35] & 0xF) << 8) | input[34];
-
-        TrackPadTouch1 = new TrackPadTouch
+        if (IsLeft)
         {
-            RawTrackingNum = input[35],
-            Id = (byte)(input[35] & 0x7f),
-            IsActive = (input[35] & 0x80) == 0,
-            X = (short)(((ushort)(input[37] & 0x0f) << 8) |
-                        input[36]),
-            Y = (short)((input[39] << 4) |
-                        ((ushort)(input[37] & 0xf0) >> 4))
-        };
-        TrackPadTouch2 = new TrackPadTouch
+            SetStick(input.Slice(6, 3));
+            LeftThumbX = _finalStickData[0];
+            LeftThumbY = _finalStickData[1];
+        }
+        else
         {
-            RawTrackingNum = input[39],
-            Id = (byte)(input[39] & 0x7f),
-            IsActive = (input[39] & 0x80) == 0,
-            X = (short)(((ushort)(input[41] & 0x0f) << 8) |
-                        input[40]),
-            Y = (short)((input[42] << 4) |
-                        ((ushort)(input[41] & 0xf0) >> 4))
-        };
-        TouchPacketCounter = input[34];
-        Touch1 = input[35] >> 7 == 0;
-        Touch2 = input[39] >> 7 == 0;
-        TouchIsOnLeftSide = !(touchX >= 1920 * 2 / 5); // TODO: port const
-        TouchIsOnRightSide = !(touchX < 1920 * 2 / 5); // TODO: port const
+            SetStick(input.Slice(9, 3));
+            RightThumbX = _finalStickData[0];
+            RightThumbY = _finalStickData[1];
+        }
     }
 
-    /// <inheritdoc />
-    public override bool IsIdle
+    private void SetStick(ReadOnlySpan<byte> stickData)
     {
-        get
-        {
-            if (!base.IsIdle)
-            {
-                return false;
-            }
+        ushort tempX = (ushort)(stickData[0] | ((stickData[1] & 0xf) << 8));
+        ushort tempY = (ushort)((stickData[1] >> 4) | (stickData[2] << 4));
 
-            if (Touch1 || Touch2 || TouchClick)
-            {
-                return false;
-            }
+        CenterSticks(tempX, tempY);
 
-            return true;
-        }
+        _finalStickData[0] = CastStickValue(_tempStickData[0]);
+        _finalStickData[1] = CastStickValue(_tempStickData[1]);
+    }
+
+    private void CenterSticks(ushort x, ushort y)
+    {
+        _tempStickData[0] = 0;
+        _tempStickData[1] = 0;
+        float dx = x - StickCalibration[2], dy = y - StickCalibration[3];
+        if (Math.Abs(dx * dx + dy * dy) < DeadZone * DeadZone)
+            return;
+
+        _tempStickData[0] = dx / (dx > 0 ? StickCalibration[0] : StickCalibration[4]);
+        _tempStickData[1] = dy / (dy > 0 ? StickCalibration[1] : StickCalibration[5]);
+    }
+
+    private static short CastStickValue(float value)
+    {
+        return (short)Math.Max(short.MinValue, Math.Min(short.MaxValue, value * (value > 0 ? short.MaxValue : -short.MinValue)));
     }
 }
