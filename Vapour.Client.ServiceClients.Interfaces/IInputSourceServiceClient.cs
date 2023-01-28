d94ecec26cf34314adb3d9ed002057a8ae1fe0fc
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2023-01-28 15:44:48 +0100

    Applied code formatting

--- Vapour.Client.ServiceClients.Interfaces/IInputSourceServiceClient.cs
+++ Vapour.Client.ServiceClients.Interfaces/IInputSourceServiceClient.cs
@@ -7,16 +7,24 @@ namespace Vapour.Client.ServiceClients;
 public interface IInputSourceServiceClient
 {
     void StartListening(CancellationToken ct = default);
+
     Task<List<InputSourceMessage>> GetInputSourceList();
-    
+
     Task SaveDefaultInputSourceConfiguration(string inputSourceKey,
         InputSourceConfiguration inputSourceConfiguration);
 
     Task<List<GameInfo>> GetGameSelectionList(string inputSourceKey, GameSource gameSource);
-    Task SaveGameConfiguration(string inputSourceKey, GameInfo gameInfo, InputSourceConfiguration inputSourceConfiguration);
+
+    Task SaveGameConfiguration(string inputSourceKey, GameInfo gameInfo,
+        InputSourceConfiguration inputSourceConfiguration);
+
     Task<List<InputSourceConfiguration>> GetGameInputSourceConfigurations(string inputSourceKey);
+
     Task DeleteGameConfiguration(string inputSourceKey, string gameId);
+
     event Action<InputSourceMessage> InInputSourceCreated;
+
     event Action<InputSourceRemovedMessage> OnInputSourceRemoved;
+
     event Action<InputSourceConfigurationChangedMessage> OnInputSourceConfigurationChanged;
 }
