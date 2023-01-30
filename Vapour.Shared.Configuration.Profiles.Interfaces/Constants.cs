18e7c79256391615d36bf08fcf9b9d34cc05fc28
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2023-01-30 11:09:28 +0100

    Applied code formatting rules

--- Vapour.Shared.Configuration.Profiles.Interfaces/Constants.cs
+++ Vapour.Shared.Configuration.Profiles.Interfaces/Constants.cs
@@ -1,9 +1,9 @@
 ﻿namespace Vapour.Shared.Configuration.Profiles;
+
 public static class Constants
 {
     /// <summary>
-    ///     The <see cref="Guid"/> identifying the default (always available) profile that is always ensured to exist.
+    ///     The <see cref="Guid" /> identifying the default (always available) profile that is always ensured to exist.
     /// </summary>
     public static readonly Guid DefaultProfileId = Guid.Parse("C74D58EA-058F-4D01-BF08-8D765CC145D1");
-
 }
