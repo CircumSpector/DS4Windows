7b176e483a79b8b4bd399fcd1e21205cdd262147
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2024-09-16 14:32:50 +0200

    Fixed build issues

--- Installer/Build.ps1
+++ Installer/Build.ps1
@@ -2,54 +2,41 @@
 #$env:APPVEYOR_BUILD_VERSION = 1.0.1
 #$env:PlATFORM = "x64"
 
+$platform = $env:PLATFORM
 $framework = "net8.0-windows10.0.17763.0"
-$publishFolder = $env:APPVEYOR_BUILD_FOLDER + "/Publish"
 $output = ($env:APPVEYOR_BUILD_FOLDER + "/Publish/x64")
+$projectRoot = $env:APPVEYOR_BUILD_FOLDER
+$publishFolder = $env:APPVEYOR_BUILD_FOLDER + "/Publish"
 
 if (Test-Path -Path $publishFolder)
 {
 	Remove-Item -Path $publishFolder -Recurse
 }
 
-Write-Host "Starting dotnet build" -ForegroundColor Green
-
-dotnet build ($env:APPVEYOR_BUILD_FOLDER + "/Vapour.sln") `
-    --configuration Release `
-    --framework $framework `
-    --runtime win-$env:PLATFORM
-
-Write-Host "Finished dotnet build" -ForegroundColor Green
-
 Write-Host "Starting dotnet publish" -ForegroundColor Green
 
-dotnet publish ($env:APPVEYOR_BUILD_FOLDER + "/Vapour.Client/Vapour.Client.csproj") `
-    --no-build `
+dotnet publish ($projectRoot + "/Vapour.Client/Vapour.Client.csproj") `
     --configuration Release `
     --framework $framework `
-    --runtime win-$env:PLATFORM `
+    --runtime win-$platform `
     --output $output `
     --self-contained false `
-    --p:PublishSingleFile=false `
-    --p:PublishReadyToRun=true
+    --p:PublishSingleFile=false
 
-dotnet publish ($env:APPVEYOR_BUILD_FOLDER + "/Vapour.Client.TrayApplication/Vapour.Client.TrayApplication.csproj") `
-    --no-build `
+dotnet publish ($projectRoot + "/Vapour.Client.TrayApplication/Vapour.Client.TrayApplication.csproj") `
     --configuration Release `
     --framework $framework `
-    --runtime win-$env:PLATFORM `
+    --runtime win-$platform `
     --output $output `
     --self-contained false `
-    --p:PublishSingleFile=false `
-    --p:PublishReadyToRun=true
+    --p:PublishSingleFile=false
 
-dotnet publish ($env:APPVEYOR_BUILD_FOLDER + "/Vapour.Server.Host/Vapour.Server.Host.csproj") `
-    --no-build `
+dotnet publish ($projectRoot + "/Vapour.Server.Host/Vapour.Server.Host.csproj") `
     --configuration Release `
     --framework $framework `
-    --runtime win-$env:PLATFORM `
+    --runtime win-$platform `
     --output $output `
     --self-contained false `
-    --p:PublishSingleFile=false `
-    --p:PublishReadyToRun=true
+    --p:PublishSingleFile=false
 
 Write-Host "Finished dotnet publish" -ForegroundColor Green
